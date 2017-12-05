6ee1ef656616ac5e98fe0da9a06d277965f46c86
Simi <Simi.Damani@austintexas.gov>
2017-12-04 18:01:18 -0600

    minor, tweak ListLink component

--- src/components/ListLink.js
+++ src/components/ListLink.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 
 class ListLink extends Component {
+
   render () {
+
+    const {id, url, text, isBoxType} = this.props;
+
     return (
       <a
-        className={ this.props.isBoxType ? "coa-list_link coa-list_link--box" : "coa-list_link" }
-        key={this.props.id}
-        href={this.props.url}
+        className={ isBoxType ? "coa-list_link coa-list_link--box" : "coa-list_link" }
+        key={id}
+        href={url}
       >
-        <span>{this.props.text}</span>
+        <span>{text}</span>
         <i className="fa fa-chevron-right" aria-hidden="true"></i>
       </a>
     );
