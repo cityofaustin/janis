3283c83639050353fd31c6b0ffde29a95d74dd68
Simi <Simi.Damani@austintexas.gov>
2017-12-04 18:08:07 -0600

    minor, conditional render

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,68 +6,60 @@ class Contact extends Component {
 
     const {phone, email, address, hours} = this.props;
 
-    return (
+    if (phone || email || address || hours) return (
 
-      <div>
+      <div className="coa-section">
+        <h4>Contact</h4>
 
-      {(phone || email || address || hours) && (
-
-        <div className="coa-section">
-          <h4>Contact</h4>
-
-        { phone && (
-          <div className="coa-section__map">
-            <h5>Phone Number</h5>
-            <a href={`tel:${phone}`}>{phone}</a>
-          </div>
-        )}
-
-        { email && (
-          <div className="coa-section__map">
-            <h5>Email</h5>
-            <a href={`mailto:${email}`}>{email}</a>
-          </div>
-        )}
-
-        { address && (
-          <div className="coa-section__map">
-            <h5>{address.name}</h5>
-            <span>{address.street}</span>
-            <span>{address.city}, {address.state} {address.zip} </span>
-            <span>{address.country}</span>
-          </div>
-        )}
-
-        { hours && (
-          <div className="coa-section__map">
-            <h5>Hours</h5>
-            <table className="usa-table-borderless">
-              <thead className="usa-sr-only">
-                <tr>
-                  <th scope="col">Day</th>
-                  <th scope="col">Open - Close Hours</th>
-                </tr>
-              </thead>
-              <tbody>
-              {
-                Object.entries(hours).map((hour, index) => {
-                  return (
-                    <tr key={index}>
-                      <th scope="row">{hour[0]}</th>
-                      <td>{hour[1]}</td>
-                    </tr>
-                  );
-                })
-              }
-              </tbody>
-            </table>
-          </div>
-
-        )}
-
+      { phone && (
+        <div className="coa-section__map">
+          <h5>Phone Number</h5>
+          <a href={`tel:${phone}`}>{phone}</a>
         </div>
-
       )}
+
+      { email && (
+        <div className="coa-section__map">
+          <h5>Email</h5>
+          <a href={`mailto:${email}`}>{email}</a>
+        </div>
+      )}
+
+      { address && (
+        <div className="coa-section__map">
+          <h5>{address.name}</h5>
+          <span>{address.street}</span>
+          <span>{address.city}, {address.state} {address.zip}</span>
+          <span>{address.country}</span>
+        </div>
+      )}
+
+      { hours && (
+        <div className="coa-section__map">
+          <h5>Hours</h5>
+          <table className="usa-table-borderless">
+            <thead className="usa-sr-only">
+              <tr>
+                <th scope="col">Day</th>
+                <th scope="col">Open - Close Hours</th>
+              </tr>
+            </thead>
+            <tbody>
+            {
+              Object.entries(hours).map((hour, index) => {
+                return (
+                  <tr key={index}>
+                    <th scope="row">{hour[0]}</th>
+                    <td>{hour[1]}</td>
+                  </tr>
+                );
+              })
+            }
+            </tbody>
+          </table>
+        </div>
+      )}
+
       </div>
     );
   }
