2f1e494589e7476d97ac69351c3038729cfe239b
Mateo Clarke <code@mateoclarke.com>
2017-12-15 14:27:06 -0600

    Add links and SVGs
    I used this method for SVG insertion https://css-tricks.com/creating-svg-icon-system-react/

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-
+import TwitterSVG from '../svg/Twitter';
+import FacebookSVG from '../svg/Facebook';
+import GithubSVG from '../svg/Github';
+import ExternalLinkSVG from '../svg/ExternalLink';
 import '../css/components/Footer.css';
 
 class Footer extends Component {
@@ -12,15 +15,25 @@ class Footer extends Component {
             <h3>AUSTIN.GOV</h3>
           </div>
           <div className="col-xs-12 col-md-6 last-md">
-            <p>
-              Austin.gov is a new website and a work in progress. For the full City of Austin website, visit austintexas.gov. Learn more about the new website at services.austin.gov.
+            <p className="coa-Footer__body-text">
+              Austin.gov is a new website and a work in progress. For the full City of Austin website, visit <a href="http://austintexas.gov" target="_blank" aria-label="Opens in new window">austintexas.gov <ExternalLinkSVG size="13"/></a>. Learn more about the new website at <a href="http://projects.austintexas.io/projects/austin-digital-services-discovery/about/what-we-are-doing/" target="_blank" aria-label="Opens in new window">projects.austintexas.io <ExternalLinkSVG size="13"/></a>.
             </p>
           </div>
           <div className="col-xs-12 col-md-6">
             <p>
-              Request 311 service or call 512-974-2000
+              Request <a href="http://311.austintexas.gov/reports/list_services" target="_blank" aria-label="Opens in new window">311 service</a> or call <a href="tel:1-512-974-2000">512-974-2000</a>
             </p>
-            <div>row of icons</div>
+            <div className="coa-Footer__icon_row">
+              <a href="https://twitter.com/austintexasgov" target="_blank" aria-label="Opens in new window">
+                <TwitterSVG size="20" />
+              </a>
+              <a href="https://www.facebook.com/austintexasgov" target="_blank" aria-label="Opens in new window">
+                <FacebookSVG size="20" />
+              </a>
+              <a href="https://github.com/cityofaustin" target="_blank" aria-label="Opens in new window">
+                <GithubSVG size="20" />
+              </a>
+            </div>
           </div>
         </div>
       </footer>
