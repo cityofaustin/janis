485ae480016f16c4da267cbc4a5748aab42d30c4
Mateo Clarke <code@mateoclarke.com>
2017-12-29 16:09:39 -0600

    PR feedback

--- src/components/I18nBanner.js
+++ src/components/I18nBanner.js
@@ -17,9 +17,6 @@ const languages = [
     title: '中文',
     abbr: 'zh',
   }, {
-    title: 'Chinese Traditional',
-    abbr: 'zh',
-  }, {
     title: 'عربى',
     abbr: 'ar',
   }, {
@@ -38,69 +35,88 @@ class I18nBanner extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeLanguage: locale().substring(0,2) || 'en', 
+      activeLanguage: locale().substring(0,2) || 'en',
     };
+    this.displayedLanguageAmount = 4;
+    this.visibleLanguageOptions = languages.slice(0, this.displayedLanguageAmount);
+    this.secondaryLanguageOptions = languages.slice(this.displayedLanguageAmount, languages.length);
   }
 
-  languageClassName = (language) => {
+  getLanguageMenuClassName = (language) => {
     const classNameBase = `coa-I18nBanner__language`;
     return language.abbr === this.state.activeLanguage
              ? `${classNameBase}--active`
              : ''
   }
 
-  otherLanguageClassName = () => {
+  getSecondaryLanguageMenuClassName = () => {
     const base = `coa-I18nBanner__other-language-options`;
     return this.state.isOpen ? `${base}--open` : `${base}--closed`;
   }
 
-  setLangauage = (e) => {
+  handleSetLanguage = (e) => {
     e.stopPropagation();
     e.nativeEvent.stopImmediatePropagation();
-    const languageAbbr = e.target.parentElement.lang;
+    const languageAbbr = e.target.lang;
+    console.log(languageAbbr)
     this.setState({
       activeLanguage: languageAbbr,
       isOpen: false,
     })
-    console.log(this.state.activeLanguage)
+  }
+
+  handleExpandMenu = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
   }
 
   render() {
-    const displayedLanguageAmount = 4
-    const visibleLanguageOptions = languages.slice(0, displayedLanguageAmount);
-    const otherLanguageOptions = languages.slice(displayedLanguageAmount, languages.length);
-
     return (
       <div className="coa-I18nBanner">
         <div className="wrapper">
           <div className="row">
-            <div className="col-md-4 col-xs coa-I18nBanner__prompt">
-              Choose your preferred language
+            <div className="col-md-4 col-xs">
+              <div className="coa-I18nBanner__prompt">
+                Choose your preferred language
+              </div>
             </div>
             <div className="col-md-8 col-xs-12">
               <ul className="coa-I18nBanner__language-list">
-                { visibleLanguageOptions.map((language, i) => {
+                { this.visibleLanguageOptions.map((language, i) => {
                     return (
-                      <li className={`coa-I18nBanner__language ${this.languageClassName(language)}`}
-                        onClick={this.setLangauage} lang={language.abbr}
+                      <li className={`coa-I18nBanner__language ${this.getLanguageMenuClassName(language)}`}
+                        onClick={this.handleSetLanguage}
                         key={i}
                       >
-                        <span className="coa-I18nBanner__language--hide-sm">{language.title}</span>
-                        <span className="coa-I18nBanner__language--hide-md">{language.abbr.toUpperCase()}</span>
+                        <span className="hidden--sm visible--md"
+                          lang={language.abbr}
+                        >
+                          {language.title}
+                        </span>
+                        <span className="hidden--md visible--sm"
+                          lang={language.abbr}
+                        >
+                          {language.abbr.toUpperCase()}
+                        </span>
                       </li>
                     )
                 })}
-                { otherLanguageOptions.length > 0 &&
-                  <li className="coa-I18nBanner__language--other" lang=""
-                    onClick={() => this.setState({ isOpen: !this.state.isOpen })}
+                { this.secondaryLanguageOptions.length > 0 &&
+                  <li className="coa-I18nBanner__language--other"
+                    onClick={this.handleExpandMenu}
                   >
-                    <span className="coa-I18nBanner__language--hide-sm">Other Language </span>
+                    <span className="hidden--sm">Other Language </span>
                     <CaretDownSVG size="20"/>
-                    <ul className={this.otherLanguageClassName()}>
-                      { otherLanguageOptions.map((language) => {
+                    <ul className={this.getSecondaryLanguageMenuClassName()}>
+                      { this.secondaryLanguageOptions.map((language) => {
                           return (
-                            <li lang={language.abbr} onClick={this.setLangauage} className={this.languageClassName(language)}>
-                              <span>{language.title}</span>
+                            <li onClick={this.handleSetLanguage}
+                              className={this.getLanguageMenuClassName(language)}
+                            >
+                              <span lang={language.abbr}>
+                                {language.title}
+                              </span>
                             </li>
                           )
                         })
