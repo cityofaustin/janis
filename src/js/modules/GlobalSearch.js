f4bce29cb94231668303f230b96d8890647dffd5
ifsimicoded <3039125+ifsimicoded@users.noreply.github.com>
2018-01-09 15:37:35 -0600

    Sd contactbug (#78)
    * updated var reference to coincide with graphql endpoint data.
    * updated home, added USWDS Search module (#80)

---
+++ src/js/modules/GlobalSearch.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class GlobalSearch extends Component {
+
+  render() {
+
+    return  (
+
+      <div role="search">
+        <form class="usa-search usa-search-big">
+          <label class="usa-sr-only" for="search-field-big">Search big</label>
+          <input id="search-field-big" type="search" name="search"/>
+          <button type="submit">
+            <span class="usa-search-submit-text">Search</span>
+          </button>
+        </form>
+      </div>
+    );
+
+  }
+}
+
+export default GlobalSearch;
