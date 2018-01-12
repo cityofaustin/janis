9f470ad842b1d6155c1d06df44b914ff1f9a8c7e
Simi <Simi.Damani@austintexas.gov>
2018-01-12 15:08:50 -0600

    minor, whitespace

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-
 import LanguageWrapper from "js/components/LanguageWrapper"
 
 import 'css/coa.css'
