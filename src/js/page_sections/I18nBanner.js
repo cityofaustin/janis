cf64c61ed59353b2be18a188b63a768679114527
Mateo Clarke <code@mateoclarke.com>
2018-01-11 22:54:20 -0600

    React intl (#84)
    Adds integration of React-Intl with React-Router v4. react-intl acts as a store holding onto language state. We could use other conveniences it provides around referencing language libraries (static files with translations), formatting for numbers/currency, and pluralization.

--- src/js/page_sections/I18nBanner.js
+++ src/js/page_sections/I18nBanner.js
@@ -1,50 +1,34 @@
-import React, { Component } from 'react';
-import locale from 'browser-locale';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
+import { SUPPORTED_LANGUAGES, SUPPORTED_LANG_CODES } from 'js/constants/languages'
 import CaretDownSVG from 'js/svg/CaretDown';
 
-const languages = [
-  {
-    title: 'English',
-    abbr: 'en',
-  }, {
-    title: 'Español',
-    abbr: 'es',
-  }, {
-    title: 'Tiếng Việt',
-    abbr: 'vn',
-  }, {
-    title: '中文',
-    abbr: 'zh',
-  }, {
-    title: 'عربى',
-    abbr: 'ar',
-  }, {
-    title: '한국어',
-    abbr: 'ko',
-  }, {
-    title: 'اردو',
-    abbr: 'ur',
-  }, {
-    title: 'မြန်မာ',
-    abbr: 'my',
-  },
-]
 
 class I18nBanner extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeLanguage: locale().substring(0,2) || 'en',
+      isOpen: false,
     };
     this.displayedLanguageAmount = 4;
-    this.visibleLanguageOptions = languages.slice(0, this.displayedLanguageAmount);
-    this.secondaryLanguageOptions = languages.slice(this.displayedLanguageAmount, languages.length);
+    this.visibleLanguageOptions = SUPPORTED_LANGUAGES.slice(0, this.displayedLanguageAmount);
+    this.secondaryLanguageOptions = SUPPORTED_LANGUAGES.slice(this.displayedLanguageAmount, SUPPORTED_LANGUAGES.length);
+    console.log(this.getPathname())
+  }
+
+  getPathname = () => {
+    let pathArray = this.props.location.pathname.split('/').filter(n => n)
+    if (SUPPORTED_LANG_CODES.includes(pathArray[0])){
+      pathArray.splice(0, 1)
+    }
+
+    return pathArray.join('/')
   }
 
   getLanguageMenuClassName = (language) => {
     const classNameBase = `coa-I18nBanner__language`;
-    return language.abbr === this.state.activeLanguage
+    return language.code === this.props.activeLanguage.substring(0,2)
              ? `${classNameBase}--active`
              : ''
   }
@@ -55,12 +39,8 @@ class I18nBanner extends Component {
   }
 
   handleSetLanguage = (e) => {
-    e.stopPropagation();
-    e.nativeEvent.stopImmediatePropagation();
-    const languageAbbr = e.target.lang;
-    console.log(languageAbbr)
+    this.props.handleManualLanguageUpdate(e.target.lang)
     this.setState({
-      activeLanguage: languageAbbr,
       isOpen: false,
     })
   }
@@ -72,6 +52,7 @@ class I18nBanner extends Component {
   }
 
   render() {
+
     return (
       <div className="coa-I18nBanner">
         <div className="wrapper">
@@ -86,19 +67,20 @@ class I18nBanner extends Component {
                 { this.visibleLanguageOptions.map((language, i) => {
                     return (
                       <li className={`coa-I18nBanner__language ${this.getLanguageMenuClassName(language)}`}
-                        onClick={this.handleSetLanguage}
-                        key={i}
+                        key={i} onClick={this.handleSetLanguage}
                       >
-                        <span className="hidden--sm visible--md"
-                          lang={language.abbr}
-                        >
-                          {language.title}
-                        </span>
-                        <span className="hidden--md visible--sm"
-                          lang={language.abbr}
-                        >
-                          {language.abbr.toUpperCase()}
-                        </span>
+                        <Link to={`/${language.code}/${this.getPathname()}`}>
+                          <span className="hidden--sm visible--md"
+                            lang={language.code}
+                            >
+                            {language.title}
+                          </span>
+                          <span className="hidden--md visible--sm"
+                            lang={language.code}
+                            >
+                            {language.abbr.toUpperCase()}
+                          </span>
+                        </Link>
                       </li>
                     )
                 })}
@@ -115,9 +97,11 @@ class I18nBanner extends Component {
                               className={this.getLanguageMenuClassName(language)}
                               key={i}
                             >
-                              <span lang={language.abbr}>
-                                {language.title}
-                              </span>
+                              <Link to={`/${language.code}/${this.getPathname()}`}>
+                                <span lang={language.code}>
+                                  {language.title}
+                                </span>
+                              </Link>
                             </li>
                           )
                         })
