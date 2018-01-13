4cc4a0dae5dffcda417e6321678917587e7cf443
Jesse Reitz <jessereitz1@gmail.com>
2018-01-12 21:53:02 -0600

    Hide hours that aren't today.

--- src/js/modules/Hours.js
+++ src/js/modules/Hours.js
@@ -3,9 +3,22 @@ import moment from 'moment';
 
 class Hours extends Component {
 
+  isToday(day) {
+    // Returns hidden classes for rows that aren't today | returns empty string for today
+    console.log('\n\n\n\n');
+    console.log(day.dayOfWeek.toLowerCase());
+    let today = moment();
+    if (day.dayOfWeek.toLowerCase() == today.format('dddd').toLowerCase())
+      return "";
+    else
+      return "hidden--sm hidden--md hidden";
+  }
+
   render() {
 
     const { hours } = this.props;
+    let today = moment();
+
 
     return (hours) && (
 
@@ -21,7 +34,8 @@ class Hours extends Component {
           <tbody>
           {
             hours.map((hour, index) =>
-              <tr key={index}>
+              // className = check to see if date is today
+              <tr key={index} className={this.isToday(hour)}>
                 <th scope="row">{hour.dayOfWeek}</th>
                 <td>{moment(hour.startTime, "HH:mm:ss").format('h:mm A')} - {moment(hour.endTime, "HH:mm:ss").format('h:mm A')}</td>
               </tr>
