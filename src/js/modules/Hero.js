866b71909ff1ba9c908b7864b0cada9bfadb07f8
Mateo Clarke <code@mateoclarke.com>
2018-01-19 01:00:46 -0600

    refactor Hero markup and css in more places

--- src/js/modules/Hero.js
+++ src/js/modules/Hero.js
@@ -3,21 +3,30 @@ import React, { Component } from 'react';
 class Hero extends Component {
 
   render() {
-    const { image } = this.props;
+    const { image, callout, home } = this.props;
     const heroImageSrc = image ?`http://localhost:8000/media/${image.file}` : '';
 
     const HeroImageStyles = {
       backgroundImage: `url(${heroImageSrc})`,
     }
 
-    return (
-      <div
-        className="coa-Hero--image"
-        style={HeroImageStyles}
-        role="img"
-        aria-label={image && image.title}
-      ></div>
-    );
+    if (image) {
+      return (
+        <div
+          className="coa-Hero--image"
+          style={HeroImageStyles}
+          role="img"
+          aria-label={image && image.title}
+        ></div>
+      )
+    } else if (callout) {
+      return (
+        <div className={`coa-Hero coa-Hero__callout ${home ? 'coa-Hero__callout--home': ''}`}>
+          <h2>{callout}</h2>
+          {this.props.children}
+        </div>
+      )
+    } else return '';
   }
 
 }
--- src/js/pages/Topic.js
+++ src/js/pages/Topic.js
@@ -5,6 +5,7 @@ import { cleanServiceLinks } from 'js/helpers/cleanData';
 
 // TODO: this jsonFileData is temporary. Add it to Wagtail API
 import jsonFileData from '__tmpdata/services';
+import Hero from 'js/modules/Hero';
 import RelatedLinks from 'js/page_sections/RelatedLinks';
 import FormFeedback from 'js/page_sections/FormFeedback';
 import Service311 from 'js/page_sections/Service311';
@@ -66,12 +67,7 @@ class Topic extends Component {
       <div>
 
         <div className="wrapper">
-          <div className="coa-main__hero">
-            <div className="coa-main__hero__callout">
-              <h2>{title}</h2>
-            </div>
-          </div>
-
+          <Hero callout={title} />
           <div className="coa-main__body" dangerouslySetInnerHTML={{__html: body}} />
         </div>
 
