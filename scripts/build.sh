e15d2f859fb066b09d4a6762e9e06739dfcacb1b
Mateo Clarke <code@mateoclarke.com>
2018-01-31 18:12:57 -0600

    update docker scripts once more

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,7 @@ TAG='janis:local'
 echo "building docker image..."
 docker build --tag "$TAG" .
 echo "running docker image..."
+HOST_IP=$(ifconfig en0 | awk '$1 == "inet" {print $2}')
 docker run \
     --rm \
     --name janis \
@@ -18,6 +19,6 @@ docker run \
     --volume "$PWD/static.config.js:/app/static.config.js" \
     --volume "$PWD/.babelrc:/app/.babelrc" \
     --volume "$PWD/yarn.lock:/app/yarn.lock" \
-    --env "CMS_API=http://$(ifconfig en0 | awk '$1 == "inet" {print $2}'):8000/api/graphql/" \
-    --env "CMS_MEDIA=http://$(ifconfig en0 | awk '$1 == "inet" {print $2}'):8000/media/" \
+    --env "CMS_API=http://$HOST_IP:8000/api/graphql/" \
+    --env "CMS_MEDIA=http://$HOST_IP:8000/media/" \
     "$TAG" yarn build
