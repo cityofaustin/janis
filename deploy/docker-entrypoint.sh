957a6ad9140c96191f500d9b531bc3aa99095eca
Ben <benweatherman@gmail.com>
2018-02-02 00:02:18 -0600

    Only substitute $PORT
    Because other vars in `nginx.conf` have a `$` in them, envsubst wants to replace those vars as well. We want to keep them so we have to specify which vars to substitute.

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -o errexit
 NGINX_CONFIG_FILE='/etc/nginx/conf.d/default.conf'
 
 echo "Setting up $NGINX_CONFIG_FILE..."
-envsubst < $NGINX_CONFIG_FILE | tee $NGINX_CONFIG_FILE
+envsubst '$PORT' < $NGINX_CONFIG_FILE | tee $NGINX_CONFIG_FILE
 
 echo "Waiting for requests..."
 exec "$@"
