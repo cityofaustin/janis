0b018dcb887f3f3daf7a9d0f2a9b68809eba158d
Ben <benweatherman@gmail.com>
2018-02-01 22:59:53 -0600

    Cleanup and allow ctrl-c to kill process
    If you don't use the `[]` version of entrypoint/cmd, docker runs them as `/bin/sh -c 'command'` instead of just executing the command. This means that `ctrl-c` gets intercepted by bash instead of by openresty. So change the calling method and we'll be able to kill openresty like usual.

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -4,7 +4,5 @@ set -o errexit
 echo "Configuring server to run on port $PORT..."
 sed "s/listen[[:blank:]]*80/listen ${PORT:-80}/" /etc/nginx/conf.d/default.conf | tee /etc/nginx/conf.d/default.conf
 
-echo "Waiting for requests...$@"
-# exec "$@"
-
-/usr/local/openresty/bin/openresty -g "daemon off;"
+echo "Waiting for requests..."
+exec "$@"
