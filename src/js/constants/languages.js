7187dc208ebbdca9e6b9dfe82831792971bc2709
Mateo Clarke <code@mateoclarke.com>
2018-02-06 14:42:38 -0600

    update supported langs for alpha based on lasted from content team

--- src/js/constants/languages.js
+++ src/js/constants/languages.js
@@ -1,48 +1,50 @@
 export const SUPPORTED_LANGUAGES = [
-  {
+  { // Default: English
     title: 'English',
     abbr: 'en',
     code: 'en',
-  }, {
+  },
+  { // Spanish
     title: 'Español',
     abbr: 'es',
     code: 'es',
-  }, {
-    // Vietnamese
+  }, 
+  { // Vietnamese
     title: 'Tiếng Việt',
     abbr: 'vi',
     code: 'vi',
-  }, {
-    // Simplified Chinese
-    title: '简体中文',
-    abbr: 'zh',
-    code: 'zh-cn',
-  }, {
-    // Traditional Chinese
-    title: '繁體中文',
-    abbr: 'zh',
-    code: 'zh-tw',
-  }, {
-    // Arabic
+  },
+  { // Arabic
     title: 'عربى',
     abbr: 'ar',
     code: 'ar',
-  }, {
-    // Korean
-    title: '한국어',
-    abbr: 'ko',
-    code: 'ko',
-  }, {
-    // Urdu
-    title: 'اردو',
-    abbr: 'ur',
-    code: 'ur',
-  }, {
-    // Burmese
-    title: 'မြန်မာ',
-    abbr: 'my',
-    code: 'my',
   },
+  //   // Simplified Chinese
+  //   title: '简体中文',
+  //   abbr: 'zh',
+  //   code: 'zh-cn',
+  // }, {
+  //   // Traditional Chinese
+  //   title: '繁體中文',
+  //   abbr: 'zh',
+  //   code: 'zh-tw',
+  // }, {
+  // {
+  //   // Korean
+  //   title: '한국어',
+  //   abbr: 'ko',
+  //   code: 'ko',
+  // }, {
+  //   // Urdu
+  //   title: 'اردو',
+  //   abbr: 'ur',
+  //   code: 'ur',
+  // }, {
+  //   // Burmese
+  //   title: 'မြန်မာ',
+  //   abbr: 'my',
+  //   code: 'my',
+  // },
 ]
 
 export const SUPPORTED_LANG_CODES = SUPPORTED_LANGUAGES.map(lang => lang.code);
