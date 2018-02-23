0a2a0c80da74dd7d335c1150ac564d1c3d4370b8
Mateo Clarke <code@mateoclarke.com>
2018-02-23 13:19:35 -0600

    fix external link to open in a new tab

--- src/js/modules/ExternalLink.js
+++ src/js/modules/ExternalLink.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ExternalLinkSVG from 'js/svg/ExternalLink'
+import ExternalLinkSVG from 'js/svg/ExternalLink';
+
+const defaultIconSize = "17";
 
 const ExternalLink = (props) => (
-  <a href={props.to} className="coa-ExternalLink">
+  <a href={props.to} className="coa-ExternalLink"
+    target="_blank" rel="noopener noreferrer" aria-label="Opens in new window"
+  >
     {props.children}
-    <ExternalLinkSVG size="17" />
+    <ExternalLinkSVG size={props.iconSize || defaultIconSize} />
   </a>
 );
 
