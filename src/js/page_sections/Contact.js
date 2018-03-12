a277382d5b41e9d4b8014fb5b77a9cce5c507f62
Simi <Simi.Damani@austintexas.gov>
2018-03-12 17:50:08 -0500

    make Contact component stateless function

--- src/js/page_sections/Contact.js
+++ src/js/page_sections/Contact.js
@@ -1,78 +1,67 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {get} from 'lodash';
 import SectionTitle from 'js/modules/SectionTitle';
 import SectionSubtitle from 'js/modules/SectionSubtitle';
 import Hours from 'js/modules/Hours';
 import StaticMap from 'js/modules/StaticMap';
 
-class Contact extends Component {
+const Contact = ({ contacts }) => {
 
-  render() {
+  if (!contacts || !contacts.length) return null;
 
-    const { contacts } = this.props;
-    let JSX;
+  return (
+    <div className="coa-section">
+      <SectionTitle title="Contact"/>
+      {
+        contacts.map((contact, index) => {
 
-    if (!contacts || !contacts.length) {
-      JSX = null;
-    } else {
-      JSX = (
-        <div className="coa-section">
-          <SectionTitle title="Contact"/>
+          const name = get(contact, "name", null);
+          const phone = get(contact, "phone", null);
+          const email = get(contact, "email", null);
+          const location = get(contact, "location", null);
+          const hours = get(contact, "hours", null);
 
-          {
-            contacts.map((contact, index) => {
+          return (
+            <div key={index} className="coa-section__subsection">
 
-              const name = get(contact, "name", null);
-              const phone = get(contact, "phone", null);
-              const email = get(contact, "email", null);
-              const location = get(contact, "location", null);
-              const hours = get(contact, "hours", null);
+                <SectionSubtitle title={name} />
 
-              return (
-                <div key={index} className="coa-section__subsection">
-
-                    <SectionSubtitle title={name} />
-
-                  { phone && (
-                    <div className="coa-section__map">
-                      <h5>Phone Number</h5>
-                      <a href={`tel:${phone}`}>{phone}</a>
-                    </div>
-                  )}
-
-                  { email && (
-                    <div className="coa-section__map">
-                      <h5>Email</h5>
-                      <a href={`mailto:${email}`}>{email}</a>
-                    </div>
-                  )}
-
-                  { location && (
-                    <div className="coa-section__map">
-                      <h5>Address</h5>
-                      <span>{location.street}</span>
-                      <span>{location.city}, {location.state} {location.zip}</span>
-                      <span>{location.country}</span>
-                    </div>
-                  )}
-
-                  { location && (
-                    <StaticMap location={location} title={`Map to ${name}`}/>
-                  )}
-
-                  <Hours hours={hours} />
-
+              { phone && (
+                <div className="coa-section__map">
+                  <h5>Phone Number</h5>
+                  <a href={`tel:${phone}`}>{phone}</a>
                 </div>
-              );
+              )}
 
-            })
-          }
-        </div>
-      );
-    }
+              { email && (
+                <div className="coa-section__map">
+                  <h5>Email</h5>
+                  <a href={`mailto:${email}`}>{email}</a>
+                </div>
+              )}
 
-    return JSX;
-  }
+              { location && (
+                <div className="coa-section__map">
+                  <h5>Address</h5>
+                  <span>{location.street}</span>
+                  <span>{location.city}, {location.state} {location.zip}</span>
+                  <span>{location.country}</span>
+                </div>
+              )}
+
+              { location && (
+                <StaticMap location={location} title={`Map to ${name}`}/>
+              )}
+
+              <Hours hours={hours} />
+
+            </div>
+          );
+
+        })
+      }
+    </div>
+  );
 }
 
 export default Contact;
