4c7388a19bd12fbb824d477aea9d0610500a47b3
Simi <Simi.Damani@austintexas.gov>
2018-03-12 17:03:31 -0500

    minor, whitespace

--- src/js/modules/ExternalLink.js
+++ src/js/modules/ExternalLink.js
@@ -4,15 +4,19 @@ import ExternalLinkSVG from 'js/svg/ExternalLink';
 
 const i18nMessages = defineMessages({
   externalLinkAriaLabel: {
-    id: 'ExternalLink.ariaLabel',
+    id: 'ExternalLink.arialabel',
     defaultMessage: 'Opens in new window',
   },
 });
 const defaultIconSize = "17";
 
 const ExternalLink = ({to, iconSize, children, intl}) => (
-  <a href={to} className="coa-ExternalLink"
-    target="_blank" rel="noopener noreferrer" aria-label={intl.formatMessage(i18nMessages.externalLinkAriaLabel)}
+  <a
+    href={to}
+    className="coa-ExternalLink"
+    target="_blank"
+    rel="noopener noreferrer"
+    aria-label={intl.formatMessage(i18nMessages.externalLinkAriaLabel)}
   >
     {children}
     <ExternalLinkSVG size={iconSize || defaultIconSize} />
