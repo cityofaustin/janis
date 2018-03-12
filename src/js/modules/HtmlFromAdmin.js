701ee6b1fc8187d7837ee5f730fdcc76231079ff
Simi <Simi.Damani@austintexas.gov>
2018-03-12 17:14:27 -0500

    make html from admin component stateless function

--- src/js/modules/HtmlFromAdmin.js
+++ src/js/modules/HtmlFromAdmin.js
@@ -1,15 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class HtmlFromAdmin extends Component {
-
-  render() {
-
-    const { content, isSection } = this.props;
-
-    return (
-      <div className="coa-HtmlFromAdmin" dangerouslySetInnerHTML={{__html: content}} />
-    );
-  }
-}
+const HtmlFromAdmin = ({content}) => (
+  <div className="coa-HtmlFromAdmin" dangerouslySetInnerHTML={{__html: content}} />
+);
 
 export default HtmlFromAdmin;
