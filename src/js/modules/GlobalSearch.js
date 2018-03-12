6a1497c3cf313df02412aa275356c4450b332b8f
Simi <Simi.Damani@austintexas.gov>
2018-03-12 17:11:23 -0500

    add i18n to global search

--- src/js/modules/GlobalSearch.js
+++ src/js/modules/GlobalSearch.js
@@ -1,31 +1,37 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { defineMessages, injectIntl } from 'react-intl';
 import SearchSVG from 'js/svg/Search';
 
-class GlobalSearch extends Component {
+const i18nMessages = defineMessages({
+  globalSearchLabel: {
+    id: 'GlobalSearch.label',
+    defaultMessage: 'Search Big',
+  },
+  globalSearchButtonText: {
+    id: 'GlobalSearch.buttontext',
+    defaultMessage: 'Search',
+  },
+});
 
-  render() {
+const GlobalSearch = ({ placeholder, intl }) => (
+  <div role="search">
+    <form className="coa-GlobalSearch usa-search usa-search-big">
+      <label className="usa-sr-only" htmlFor="search-field-big">
+        {intl.formatMessage(i18nMessages.globalSearchLabel)}
+      </label>
+      <input id="search-field-big" type="search" name="search"
+        placeholder={placeholder}
+      />
+      <button type="submit" className="coa-GlobalSearch__button">
+        <span className="usa-search-submit-text">
+          {intl.formatMessage(i18nMessages.globalSearchButtonText)}
+        </span>
+        <span className="coa-GlobalSearch__icon">
+          <SearchSVG size="20"/>
+        </span>
+      </button>
+    </form>
+  </div>
+);
 
-    return  (
-
-      <div role="search">
-        <form className="coa-GlobalSearch usa-search usa-search-big">
-          <label className="usa-sr-only" htmlFor="search-field-big">Search big</label>
-          <input id="search-field-big" type="search" name="search"
-            placeholder={this.props.placeholder}
-          />
-          <button type="submit" className="coa-GlobalSearch__button">
-            <span className="usa-search-submit-text">
-              Search
-            </span>
-            <span className="coa-GlobalSearch__icon">
-              <SearchSVG size="20"/>
-            </span>
-          </button>
-        </form>
-      </div>
-    );
-
-  }
-}
-
-export default GlobalSearch;
+export default injectIntl(GlobalSearch);
