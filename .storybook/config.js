b0d3658db4c6ead56c6236d400b3e81fd8a30fce
Mateo Clarke <code@mateoclarke.com>
2018-03-14 15:24:14 -0500

    fix config and load first component

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
 import { configure } from '@storybook/react';
+import 'css/coa.css'
+
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
+const req = require.context('../src/stories', true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach((filename) => req(filename));
 }
