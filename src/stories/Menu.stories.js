4c10aca5dcf91d15b9ff1821825313f5b3ae488a
Mateo Clarke <code@mateoclarke.com>
2018-03-16 14:06:25 -0500

    add spiffy a11y & viewport addons for storybook

--- src/stories/Menu.stories.js
+++ src/stories/Menu.stories.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { checkA11y } from '@storybook/addon-a11y';
 import { Route, Link, MemoryRouter } from 'react-router-dom';
 
 import Menu from 'js/page_sections/Menu';
 
 
 storiesOf('Menu', module)
+  .addDecorator(checkA11y)
   .addDecorator(story => (
     <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
   ))
