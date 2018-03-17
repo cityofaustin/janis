fed194d30615c681c4961aa2b53fcf548e86b8b0
Ben <benweatherman@gmail.com>
2018-03-16 22:14:09 -0500

    Updates based on CMS changes
    - Update `content` => `steps`
    - Update `extra_content` => `dynamic_content`
    - Update `extra_content.content` => `additional_content`
    - Changed homepage image name

--- src/js/page_sections/ContentItems.js
+++ src/js/page_sections/ContentItems.js
@@ -5,12 +5,7 @@ import ApplicationBlock from 'js/page_sections/ApplicationBlock';
 const ContentItems = ({ contentItems }) => {
   if (!contentItems || !contentItems.length) return null;
   return contentItems.map((content) => {
-    let JSX;
-    if (content.type === 'content') {
-      JSX = <HtmlFromAdmin content={content.value} />;
-    } else {
-      JSX = <ApplicationBlock type={content.type} data={content.value} />;
-    }
+    let JSX = <ApplicationBlock type={content.type} data={content.value} />;
     return <div key={content.id} className="coa-section">{JSX}</div>;
   });
 }
