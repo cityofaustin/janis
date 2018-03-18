4e041916f1b9d47fc96ca5e8e3eac349d0d9549a
Simi <Simi.Damani@austintexas.gov>
2018-03-18 18:29:56 -0500

    make breadcrumbs responsive

--- src/js/modules/PageBreadcrumbs.js
+++ src/js/modules/PageBreadcrumbs.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import I18nNavLink from 'js/modules/I18nNavLink';
 
-const PageBreadcrumbs = ({ breadcrumbs=[] }) => {
+const PageBreadcrumbs = (props) => {
+
+  const { title, order, ...rest } = props;
+
+  const breadcrumbs = order.map(breadcrumb => ({
+    className: breadcrumb,
+    title: rest[breadcrumb].title,
+    slug: rest[breadcrumb].slug,
+  }));
+
   breadcrumbs.unshift({
+    className: 'home',
     title: 'Home',
     slug: '/',
   });
 
-  const JSX = breadcrumbs.map(breadcrumb =>
+  const JSX = breadcrumbs.map((breadcrumb, index) =>
     <I18nNavLink
-      to={breadcrumb.slug}>
+      key={index}
+      className={`coa-PageBreadcrumbs__${breadcrumb.className}`}
+      to={breadcrumb.slug}
+    >
       {breadcrumb.title}
     </I18nNavLink>
   );
 
   return (
-    <div className="coa-PageBreadcrumbs wrapper">{JSX}</div>
+    <div className="wrapper container-fluid">
+      <div className="coa-PageBreadcrumbs">
+        {JSX}
+        <span className="coa-PageBreadcrumbs__title">{title}</span>
+      </div>
+    </div>
   );
 }
 
