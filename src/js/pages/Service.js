0a19ad41dc400f74a9d213f88a6fe5ff72b65401
Simi <Simi.Damani@austintexas.gov>
2018-03-18 22:03:49 -0500

    page banner, page header, page breadcrumbs, steps on service page

--- src/js/pages/Service.js
+++ src/js/pages/Service.js
@@ -4,14 +4,17 @@ import { defineMessages, injectIntl } from 'react-intl';
 import { cleanContacts, cleanRelatedServiceLinks } from 'js/helpers/cleanData';
 
 import PageBanner from 'js/modules/PageBanner';
+import PageBreadcrumbs from 'js/modules/PageBreadcrumbs';
+import PageHeader from 'js/modules/PageHeader';
+import Steps from 'js/modules/Steps';
+
 import ContentItems from 'js/page_sections/ContentItems';
 import Contact from 'js/page_sections/Contact';
 import RelatedLinks from 'js/page_sections/RelatedLinks';
 import Service311 from 'js/page_sections/Service311';
 import HtmlFromAdmin from 'js/modules/HtmlFromAdmin';
-import I18nLink from 'js/modules/I18nLink';
 
-import jsonFileData from '__tmpdata/services';
+import jsonFileData from '__tmpdata/pages';
 
 const i18nMessages = defineMessages({
   serviceRelatedlinksSectionheader: {
@@ -21,30 +24,24 @@ const i18nMessages = defineMessages({
 });
 
 const Service = ({ service }) => {
-  const { topic={}, image, title, steps, extraContent, contacts, related } = service;
-  const { services311 } = jsonFileData;
+  const { image, title, slug, content, extraContent, contacts, related } = service;
+
+  //TODO: data below should be sourced as above
+  const { servicepage, services311 } = jsonFileData;
+  const { theme, topic } = servicepage;
+
+  //TODO: clean data where sourced
   const cleanedContacts = cleanContacts(contacts);
   const cleanedRelated = cleanRelatedServiceLinks(related);
 
   return (
     <div>
       <PageBanner image={image} />
-
-              { topic.id && (
-                <I18nLink className="coa-main__breadcrumb"
-                  to={`/topics/${topic.id}`}>
-                  {topic.name}
-                </I18nLink>
-              )}
-
-              <h2 className="coa-main__title">{title}</h2>
-
-              { steps && (
-                <div className="coa-main__steps">
-                  <HtmlFromAdmin content={steps} />
-                </div>
-              )}
-
+      <PageBreadcrumbs title={title} order={['theme', 'topic']} theme={theme} topic={topic} />
+      <div className="wrapper wrapper--sm container-fluid">
+        <PageHeader title={title} />
+        { content && <Steps steps={content} /> }
+      </div>
             <ContentItems contentItems={extraContent} />
 
             <Contact contacts={contacts} />
