57fdf73ecaa5ed77d2814c3187ec9fe28d0a8f1e
Simi <Simi.Damani@austintexas.gov>
2018-03-18 19:18:56 -0500

    Page header jsx, styles, default h1 styles, default font sizes

--- src/js/modules/PageBanner.js
+++ src/js/modules/PageBanner.js
@@ -1,18 +1,14 @@
 import React from 'react';
 
-const PageBanner = ({ image }) => {
-  if(!image) return;
-
-  return (
-    <div
-      className="coa-PageBanner"
-      style={{
-        backgroundImage: `url(${process.env.CMS_MEDIA}/${image.file})`
-      }}
-      role="img"
-      aria-label={image.title}
-    />
-  );
-}
+const PageBanner = ({ image }) => (
+  <div
+    className="coa-PageBanner"
+    style={{
+      backgroundImage: `url(${process.env.CMS_MEDIA}/${image.file})`
+    }}
+    role="img"
+    aria-label={image.title}
+  />
+)
 
 export default PageBanner;
---
+++ src/js/modules/PageHeader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PageHeader = ({ title }) => (
+  <h1 className="coa-PageHeader">{title}</h1>
+);
+
+export default PageHeader;
