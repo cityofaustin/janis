b669ac2ece64c061f654b875c67f6c9667d72a0c
Simi <Simi.Damani@austintexas.gov>
2018-03-21 02:03:36 -0500

    oops -- add intl ref to steps

--- src/js/modules/Steps.js
+++ src/js/modules/Steps.js
@@ -8,11 +8,11 @@ const i18nMessages = defineMessages({
   }
 });
 
-const Steps = ({ steps }) => (
+const Steps = ({ steps, intl }) => (
   <div className="coa-Steps">
     <h2 className="coa-Steps__title">{intl.formatMessage(i18nMessages.stepsSectionTitle)}</h2>
     <div className="coa-Steps__list" dangerouslySetInnerHTML={{__html: steps}} />
   </div>
 );
 
-export default Steps;
+export default injectIntl(Steps);
