9f41fe83ce3450900655d1be58f4d25521830943
Mateo Clarke <code@mateoclarke.com>
2018-03-21 00:25:29 -0500

    updates to new mockup

--- src/js/page_sections/Footer.js
+++ src/js/page_sections/Footer.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, defineMessages, injectIntl } from 'react-intl';
+
 import TwitterSVG from 'js/svg/Twitter';
 import FacebookSVG from 'js/svg/Facebook';
 import GithubSVG from 'js/svg/Github';
 import ExternalLink from 'js/modules/ExternalLink';
+import citySealImg from 'images/coa_seal_white.png';
 
-const Footer = (props) => (
+const i18nMessages = defineMessages({
+  sealAltText: {
+    id: 'Footer.sealAltText',
+    defaultMessage: 'City of Austin Seal',
+  },
+});
+
+const Footer = ({ intl }) => (
   <footer className="coa-Footer">
     <div className="container-fluid wrapper">
       <div className="coa-section">
         <div className="row">
-          <div className="col-xs-12">
-            <h3>AUSTIN.GOV</h3>
+          <div className="col-xs-12 col-md-2">
+            <div className="coa-Footer__city-seal-wrapper">
+              <img src={citySealImg} alt={intl.formatMessage(i18nMessages.sealAltText)} />
+            </div>
           </div>
-          <div className="col-xs-12 col-md-6 last-md">
+          <div className="col-xs-12 col-md-4">
             <p className="coa-Footer__body-text">
               <FormattedMessage
                 id="Footer.bodytext"
-                defaultMessage="Austin.gov is a new website and a work in progress. For the full City of Austin website, visit {citySiteLink}. Learn more about the new website at {projectsSiteLink}."
+                defaultMessage="Alpha.austin.gov is a new website and a work in progress. For the full City of Austin website, visit {citySiteLink}. Learn more about the new website at {projectsSiteLink}."
                 values ={{
                   citySiteLink: <ExternalLink to="http://austintexas.gov" iconSize="small">austintexas.gov</ExternalLink>,
                   projectsSiteLink: <ExternalLink to="http://projects.austintexas.io/projects/austin-digital-services-discovery/about/what-we-are-doing/" iconSize="small">projects.austintexas.io</ExternalLink>
@@ -25,17 +36,7 @@ const Footer = (props) => (
               />
             </p>
           </div>
-          <div className="col-xs-12 col-md-6">
-            <p>
-              <FormattedMessage
-                id="Footer.311text"
-                defaultMessage="Request services at {servicesLink} or call {phoneLink}."
-                values = {{
-                  servicesLink: <ExternalLink to="http://311.austintexas.gov/reports/list_services" iconSize="small">311.austintexas.gov</ExternalLink>,
-                  phoneLink: <a href="tel:1-512-974-2000">512-974-2000</a>
-                }}
-              />
-            </p>
+          <div className="col-xs-12 col-md-2 col-md-offset-4">
             <div className="coa-Footer__icons">
               <ExternalLink to="https://twitter.com/austintexasgov" noIcon={true}>
                 <TwitterSVG size="20" />
@@ -54,4 +55,4 @@ const Footer = (props) => (
   </footer>
 );
 
-export default Footer;
+export default injectIntl(Footer);
