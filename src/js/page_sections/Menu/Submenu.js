9106c726ce03741060a80d20d81f1d8a8bb49db3
Simi <Simi.Damani@austintexas.gov>
2018-03-26 16:38:22 -0500

    use language constants to determine submenu alignment

--- src/js/page_sections/Menu/Submenu.js
+++ src/js/page_sections/Menu/Submenu.js
@@ -1,52 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
-import { defineMessages, injectIntl } from 'react-intl';
+import { FormattedMessage, injectIntl } from 'react-intl';
+import { find } from 'lodash';
+import { SUPPORTED_LANGUAGES } from 'js/i18n/constants';
 
 import SubmenuItem from 'js/page_sections/Menu/SubmenuItem';
 import ExternalLink from 'js/modules/ExternalLink';
 import I18nNavLink from 'js/modules/I18nNavLink';
 import ArrowRightSVG from 'js/svg/ArrowRight';
 
-const getSubmenuClassnames = (intl, id, openSection) => {
-  const base = 'coa-Submenu';
-  const arabicRightMenuItems = intl.locale === 'ar' && id < 5;
-  const nonArabicRightMenuItems = intl.locale !== 'ar' && id > 4;
-  const shouldAlignRight = arabicRightMenuItems ||  nonArabicRightMenuItems;
-  const alignRight = shouldAlignRight ? 'coa-Submenu--align-right' : '';
-  const open = openSection === id ? 'coa-Submenu--open' : '';
+const SUBMENU_THRESHOLD_ALIGNRIGHT_RTL = 5;
+const SUBMENU_THRESHOLD_ALIGNRIGHT_LTR = 4;
 
-  return `${base} ${alignRight} ${open}`;
+const isAlignedRight = (direction, id) => {
+  return (direction === 'rtl') ? id < SUBMENU_THRESHOLD_ALIGNRIGHT_RTL : id > SUBMENU_THRESHOLD_ALIGNRIGHT_LTR;
+};
+
+const isMenuOpen = (openSection, id) => {
+  return openSection === id;
 }
 
-const Submenu = ({id, openSection, theme, handleMenuToggle, intl}) => (
-  <ul className={getSubmenuClassnames(intl, id, openSection)}
-    id={`topicMenu${id+1}`}
-    role="menu"
-    aria-labelledby={`theme${id+1}`}
-  >
-  {
-    theme.topics.edges.map(({ node: topic }, topicId) => {
-      return topic.slug !== "false" && (
-        <SubmenuItem
-          key={topicId}
-          topic={topic}
-          handleClick={handleMenuToggle}
-        />
-      );
-    })
-  }
-    <li key={id} className="coa-SubmenuItem coa-SubmenuItem--theme" role="menuitem">
-      <I18nNavLink to={`/theme/${theme.slug}`}>
-        {theme.title}
-        <span className="coa-SubmenuItem__arrow-right">
-          <ArrowRightSVG size="13" />
-        </span>
-      </I18nNavLink>
-    </li>
-    <WorkInProgressSubitem />
-  </ul>
-);
+const Submenu = ({id, openSection, theme, handleMenuToggle, intl}) => {
+
+  const langObj = find(SUPPORTED_LANGUAGES, {'code': intl.locale});
+  return (
+    <ul className={`coa-Submenu
+        ${isAlignedRight(langObj.direction, id) && 'coa-Submenu--align-right'}
+        ${isMenuOpen(openSection, id) && 'coa-Submenu--open'}
+      `}
+      id={`topicMenu${id+1}`}
+      role="menu"
+      aria-labelledby={`theme${id+1}`}
+    >
+    {
+      theme.topics.edges.map(({ node: topic }, topicId) => {
+        return topic.slug !== "false" && (
+          <SubmenuItem
+            key={topicId}
+            topic={topic}
+            handleClick={handleMenuToggle}
+          />
+        );
+      })
+    }
+      <li key={id} className="coa-SubmenuItem coa-SubmenuItem--theme" role="menuitem">
+        <I18nNavLink to={`/theme/${theme.slug}`}>
+          {theme.title}
+          <span className="coa-SubmenuItem__arrow-right">
+            <ArrowRightSVG size="13" />
+          </span>
+        </I18nNavLink>
+      </li>
+      <WorkInProgressSubitem />
+    </ul>
+  );
+}
 
 // MenuItem.propTypes = {
 //   : PropTypes.
