bc14ce81790ac4f9e0ea08455a1b245f8fe52f38
Simi <simi.damani@austintexas.gov>
2018-04-11 21:37:51 -0500

    add proptypes

--- src/js/modules/Contact/ContactMap.js
+++ src/js/modules/Contact/ContactMap.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import StaticMap from 'js/modules/StaticMap';
 import Address from 'js/modules/Contact/Address';
 import Phone from 'js/modules/Contact/Phone';
@@ -26,5 +27,14 @@ const ContactMap = ({ contact:{name, phone, hours, location} }) => (
     )}
   </div>
 );
+
+ContactMap.propTypes = {
+  contact: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    location: PropTypes.object.isRequired,
+    hours: PropTypes.array,
+    phone: PropTypes.object,
+  }).isRequired,
+};
 
 export default ContactMap;
