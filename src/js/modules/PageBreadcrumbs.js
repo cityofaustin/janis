3edd0bab4d45c43aa284f3ddf46a59ed265f5129
Simi <simi.damani@austintexas.gov>
2018-04-11 23:16:24 -0500

    unnecessary code cleaning

--- src/js/modules/PageBreadcrumbs.js
+++ src/js/modules/PageBreadcrumbs.js
@@ -3,6 +3,13 @@ import { defineMessages, injectIntl } from 'react-intl';
 import I18nLink from 'js/modules/I18nLink';
 import PropTypes from 'prop-types';
 
+const i18nMessages = defineMessages({
+  home: {
+    id: 'PageBreadcrumbs.Home.text',
+    defaultMessage: 'Home',
+  }
+});
+
 const Breadcrumb = ({ breadcrumb, classNameSuffix }) => (
   <I18nLink
     className={`coa-PageBreadcrumbs__${classNameSuffix}`}
@@ -15,31 +22,21 @@ const Breadcrumb = ({ breadcrumb, classNameSuffix }) => (
   </I18nLink>
 );
 
-const PageBreadcrumbs = ({ intl, title, grandparent, parent }) => {
-
-  const i18nMessages = defineMessages({
-    home: {
-      id: 'PageBreadcrumbs.Home.text',
-      defaultMessage: 'Home',
-    }
-  });
-
-  const home = ({
-    text: intl.formatMessage(i18nMessages.home),
-    slug: '',
-  });
-
-  return (
-    <div className="wrapper container-fluid">
-      <div className="coa-PageBreadcrumbs">
-        <Breadcrumb breadcrumb={home} classNameSuffix="home" />
-        { grandparent && <Breadcrumb breadcrumb={grandparent} classNameSuffix="grandparent" />}
-        { parent && <Breadcrumb breadcrumb={parent} classNameSuffix="parent" />}
-        <span className="coa-PageBreadcrumbs__title">{title}</span>
-      </div>
+const PageBreadcrumbs = ({ intl, title, grandparent, parent }) => (
+  <div className="wrapper container-fluid">
+    <div className="coa-PageBreadcrumbs">
+      <Breadcrumb breadcrumb={{
+          text: intl.formatMessage(i18nMessages.home),
+          slug: '',
+        }}
+        classNameSuffix="home"
+      />
+      { grandparent && <Breadcrumb breadcrumb={grandparent} classNameSuffix="grandparent" />}
+      { parent && <Breadcrumb breadcrumb={parent} classNameSuffix="parent" />}
+      <span className="coa-PageBreadcrumbs__title">{title}</span>
     </div>
-  );
-}
+  </div>
+);
 
 PageBreadcrumbs.propTypes = {
   title: PropTypes.string,
