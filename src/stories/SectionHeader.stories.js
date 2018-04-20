1c520f7805d05696879bd3795c18200f06e272d9
Simi <simi.damani@austintexas.gov>
2018-04-19 22:51:15 -0500

    Section Header storybook

--- src/stories/SectionHeader.stories.js
+++ src/stories/SectionHeader.stories.js
@@ -2,12 +2,26 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import SectionHeader from 'js/modules/SectionHeader';
+import ArrowRight from 'js/svg/ArrowRight';
 
 storiesOf('SectionHeader', module)
   .add('SectionHeader', () => (
     <div className="wrapper container-fluid">
-      <SectionHeader
-        title="Lorem ipsum dolor sit amet cume"
-      />
+      <SectionHeader>Lorem ipsum dolor sit amet cume</SectionHeader>
+    </div>
+  ))
+  .add('SectionHeader highlight', () => (
+    <div className="wrapper container-fluid">
+      <SectionHeader hasHighlight={true}>Lorem ipsum dolor sit amet cume</SectionHeader>
+    </div>
+  ))
+  .add('SectionHeader link', () => (
+    <div className="wrapper container-fluid">
+      <SectionHeader hasHighlight={true}><a href="#">Lorem ipsum dolor sit amet cume&nbsp;<ArrowRight /></a></SectionHeader>
+    </div>
+  ))
+  .add('SectionHeader serif', () => (
+    <div className="wrapper container-fluid">
+      <SectionHeader isSerif={true}>Lorem ipsum dolor sit amet cume</SectionHeader>
     </div>
   ))
