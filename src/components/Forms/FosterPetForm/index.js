ab72b22a931c2f2316fbcd3ed89669b7f5e20a11
Mateo Clarke <code@mateoclarke.com>
2018-05-03 14:14:11 -0500

    Add sidebar to form

--- src/components/Forms/FosterPetForm/index.js
+++ src/components/Forms/FosterPetForm/index.js
@@ -4,6 +4,9 @@ import { defineMessages, injectIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 
 import PageHeader from 'components/PageHeader';
+import SectionHeader from 'components/SectionHeader';
+import Email from 'components/Contact/Email';
+import Phone from 'components/Contact/Phone';
 
 import Form from 'react-jsonschema-form';
 
@@ -75,19 +78,28 @@ const log = type => console.log.bind(console, type);
 const FosterPetForm = ({}) => {
   return (
     <div className="wrapper container-fluid">
-      <PageHeader
-        title="Austin Animal Center Foster Care Application"
-        hasBorder={true}
-        description="test form using react-jsonschema-form"
-      />
-
-      <Form
-        schema={schema}
-        uiSchema={uiSchema}
-        onChange={log('changed')}
-        onSubmit={log('submitted')}
-        onError={log('errors')}
-      />
+      <div className="row">
+        <PageHeader
+          title="Austin Animal Center Foster Care Application"
+          hasBorder={true}
+          description="test form using react-jsonschema-form"
+        />
+        <div className="col-md-8">
+          <Form
+            schema={schema}
+            uiSchema={uiSchema}
+            onChange={log('changed')}
+            onSubmit={log('submitted')}
+            onError={log('errors')}
+          />
+        </div>
+        <div className="col-md-4">
+          <SectionHeader>Contact</SectionHeader>
+          <h3>Foster Care Coordinators</h3>
+          <Email email="animal.foster@austintexas.gov" />
+          <Phone phone={{ default: '512-978-0541' }} />
+        </div>
+      </div>
     </div>
   );
 };
