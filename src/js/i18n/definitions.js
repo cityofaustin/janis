dbc1db3fda8ef8ed01c18daa30d12bd5a763704e
Simi <simi.damani@austintexas.gov>
2018-05-03 15:36:36 -0500

    update hours translations. fix hours sorting by current day. show all hours even if data from joplin isn't present

--- src/js/i18n/definitions.js
+++ src/js/i18n/definitions.js
@@ -4,3 +4,17 @@ export const call_to_action = defineMessages({
   enter_address: 'Type your street address in the box below',
   what_do_i_do_with: 'Check the "What do I do with" tool below to find out what items are accepted.',
 });
+
+export const date = defineMessages({
+  weekday_sunday: 'sunday',
+  weekday_monday: 'monday',
+  weekday_tuesday: 'tuesday',
+  weekday_wednesday: 'wednesday',
+  weekday_thursday: 'thursday',
+  weekday_friday: 'friday',
+  weekday_saturday: 'saturday',
+});
+
+export const contact = defineMessages({
+  closed: 'closed'
+})
