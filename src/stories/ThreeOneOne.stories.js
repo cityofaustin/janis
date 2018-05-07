edca10d6cbd6f2bafe7ff4e707ebb412522e1ade
Simi <simi.damani@austintexas.gov>
2018-05-07 15:42:49 -0500

    fix 311 storybook

--- src/stories/ThreeOneOne.stories.js
+++ src/stories/ThreeOneOne.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import threeoneoneData from 'stories/static_data/threeoneoneData';
+import ThreeOneOne from 'components/PageSections/ThreeOneOne';
 
 storiesOf('ThreeOneOne', module).add('ThreeOneOne', () => (
   <div className="wrapper container-fluid">
