d6dc302bc8b177b676b6684a808798058e0bc78d
Brian Smith <briaguya@gmail.com>
2018-05-21 15:48:20 -0500

    move away from background-url

--- src/components/HeroHome/index.js
+++ src/components/HeroHome/index.js
@@ -1,22 +1,17 @@
 import React from 'react';
 
 const HeroHome = ({ imageUrl, imageTitle, preheader }) => (
-  <div
-    className="coa-HeroHome"
-    style={{
-      backgroundImage: `
-        linear-gradient(rgba(36, 11, 51, .3), rgba(36, 11, 51, .3)),
-        url(${imageUrl})
-      `,
-      backgroundSize: 'cover',
-      backgroundPosition: '50%',
-    }}
-    role="img"
-    aria-label={imageTitle}
-  >
-    <div className="container-fluid wrapper">
-      <span className="coa-HeroHome-preheader">{preheader}</span>
-      <h2 className="coa-HeroHome-header">Austin, TX</h2>
+  <div className="coa-HeroHome-container">
+    <img src={imageUrl} />
+    <div
+      className="coa-HeroHome"
+      role="img"
+      aria-label={imageTitle}
+    >
+      <div className="container-fluid wrapper">
+        <span className="coa-HeroHome-preheader">{preheader}</span>
+        <h2 className="coa-HeroHome-header">Austin, TX</h2>
+      </div>
     </div>
   </div>
 );
