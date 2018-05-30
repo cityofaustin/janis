1e822134a7fcad18dc37904dd9d73527e061787a
Brian Smith <briaguya@gmail.com>
2018-05-30 13:19:18 -0500

    fix pagebanner storybook

--- src/stories/PageBanner.stories.js
+++ src/stories/PageBanner.stories.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import PageBanner from 'components/PageBanner';
-import dummyImage from 'stories/static_data/images/pagebanner_test.jpg';
 
 storiesOf('PageBanner', module).add('PageBanner', () => (
-  <div className="wrapper container-fluid">
-    <PageBanner imageUrl={dummyImage} imageTitle={'test'} />
-  </div>
+  <PageBanner
+    imagesPath="/images/"
+    imageFilename="pagebanner_test"
+    imageExtension=".jpg"
+    imageTitle={'test'}
+  />
 ));
