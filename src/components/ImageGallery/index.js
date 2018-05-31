9fa9bb1156efe7688843d65c2f90759c87a94d81
Brian Smith <briaguya@gmail.com>
2018-05-31 02:10:52 -0500

    couple more responsive parts

--- src/components/ImageGallery/index.js
+++ src/components/ImageGallery/index.js
@@ -1,28 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import MediaQuery from 'react-responsive';
 import ResponsiveImage from 'components/ResponsiveImage';
 import { IMAGE_GALLERY_RESPONSIVE_IMAGE_SIZES } from 'js/helpers/constants';
+
+const smallScreen = 479;
+const mediumScreen = 767;
+const largeScreen = 1079;
+const xlargeScreen = 1249;
+
+const maxMediumScreenItems = 2;
+const maxLargeScreenItems = 3;
+
+const ImageItem = ({ image, imagekey }) => (
+  <div className="coa-ImageGallery__imageitem">
+    <ResponsiveImage
+      key={imagekey}
+      filename={image.filename}
+      extension={image.extension}
+      widths={IMAGE_GALLERY_RESPONSIVE_IMAGE_SIZES}
+    />
+    <div className="coa-ImageGallery__imageitem-description">
+      <p>{image.description}</p>
+    </div>
+  </div>
+);
 
 const ImageGallery = ({ heading, subheading, images }) => (
   <div className="coa-ImageGallery__container">
     <h2>{heading}</h2>
     <p>{subheading}</p>
     <div className="coa-ImageGallery__images">
-      {images.length > 3 && <div>LEFT</div>}
-      {images.map((image, key) => (
-        <div className="coa-ImageGallery__imageitem">
-          <ResponsiveImage
-            key={key}
-            filename={image.filename}
-            extension={image.extension}
-            widths={IMAGE_GALLERY_RESPONSIVE_IMAGE_SIZES}
-          />
-          <div className="coa-ImageGallery__imageitem-description">
-            <p>{image.description}</p>
-          </div>
-        </div>
-      ))}
-      {images.length > 3 && <div>RIGHT</div>}
+      <MediaQuery maxWidth={mediumScreen}>
+        {images.map((image, key) => <ImageItem image={image} imagekey={key} />)}
+      </MediaQuery>
+      <MediaQuery minDeviceWidth={mediumScreen} maxWidth={largeScreen}>
+        {images.length > maxMediumScreenItems && <div>LEFT</div>}
+        {images.length > maxMediumScreenItems && <div>RIGHT</div>}
+      </MediaQuery>
+      <MediaQuery minDeviceWidth={largeScreen}>
+        {images.length > maxLargeScreenItems && <div>LEFT</div>}
+        {images.length > maxLargeScreenItems && <div>RIGHT</div>}
+      </MediaQuery>
     </div>
   </div>
 );
