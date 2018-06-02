4d13b0c803ebd59e1020b8fb6f563f26a2952e3b
Dinh Huynh <dinh_huynh4512@utexas.edu>
2018-06-02 12:03:46 -0500

    added downShift

--- src/components/Trashy/index.js
+++ src/components/Trashy/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import TrashySchedule from 'components/Trashy/TrashySchedule';
+import TrashyAddress from 'components/Trashy/TrashyAddress';
+
 
 const Trashy = () => (
   <div>
     <div>TODO: Implement the Recycling Schedule component.</div>
     <TrashySchedule />
+    <TrashyAddress />
   </div>
+
 );
 
 export default Trashy;
