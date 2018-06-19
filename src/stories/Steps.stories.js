56394eb42b18d83298d23b15b355b7ee88653c84
Simi <simi.damani@austintexas.gov>
2018-06-19 01:48:02 -0500

    make step data and stepdetailgroup stories

--- src/stories/Steps.stories.js
+++ src/stories/Steps.stories.js
@@ -3,27 +3,21 @@ import { storiesOf } from '@storybook/react';
 
 import Steps from 'components/Steps';
 import StepDetail from 'components/Steps/StepDetail';
+import StepDetailGroup from 'components/Steps/StepDetailGroup';
 
-const stepsHtml = `
-  <ul>
-    <li><p>Check the “What do I do with” tool below to find out what items are accepted. Some items are free to drop off and some items, like tires, require a fee.</p></li>
-    <li><p>Review the Household Hazardous Waste Do’s and Don’ts below.</p></li>
-    <li><p>Drop off your items at the Recycle and ReUse Center at 2514 Business Center Dr, Austin, TX 78744.</p></li>
-  </ul>
-`;
+import { stepData, stepDetailGroupData } from 'stories/static_data/stepData';
 
 storiesOf('Steps', module)
   .add('Steps', () => (
     <div className="wrapper container-fluid">
-      <Steps stepsAsHtmlFromAdmin={stepsHtml} />
+      <Steps stepsAsHtmlFromAdmin={stepData} />
     </div>
   ))
   .add('Step Detail', () => (
     <div className="wrapper container-fluid">
-      <StepDetail
-        title="1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
-        content="<ul><li>While you can get a foster animal before completing this step, you will need to attend a foster program orientation within three months of becoming approved to foster. You will learn about shelter operations and the important role that the foster program and foster care providers play</li><li>some detail step here</li><li>some detail step here</li></ul>"
-        link={{ text: 'Link To Somewhere', url: '#' }}
-      />
+      <StepDetail {...stepDetailGroupData[0]} />
     </div>
+  ))
+  .add('Step Detail Group', () => (
+    <StepDetailGroup steps={stepDetailGroupData} stepClassName="wrapper--sm" />
   ));
---
+++ src/stories/static_data/stepData.js
@@ -0,0 +1,37 @@
+export const stepData = `
+  <ul>
+    <li><p>Check the “What do I do with” tool below to find out what items are accepted. Some items are free to drop off and some items, like tires, require a fee.</p></li>
+    <li><p>Review the Household Hazardous Waste Do’s and Don’ts below.</p></li>
+    <li><p>Drop off your items at the Recycle and ReUse Center at 2514 Business Center Dr, Austin, TX 78744.</p></li>
+  </ul>
+`;
+
+export const stepDetailGroupData = [
+  {
+    title: '1. Before you apply',
+    link: {
+      text: 'Go to before you apply',
+      url: '#',
+    },
+    content:
+      '<ul><li><p>Learn how to foster pets with Austin Animal Center.</p></li><li><p>Learn what types of animals need fostering.</p></li><li><p>Verify your current pets’ rabies vaccinations with your veterinarian.</p></li></ul>',
+  },
+  {
+    title: '2. Before you apply',
+    link: {
+      text: 'Go to before you apply',
+      url: '#',
+    },
+    content:
+      '<ul><li><p>Learn how to foster pets with Austin Animal Center.</p></li><li><p>Learn what types of animals need fostering.</p></li><li><p>Verify your current pets’ rabies vaccinations with your veterinarian.</p></li></ul>',
+  },
+  {
+    title: '3. Before you apply',
+    link: {
+      text: 'Go to before you apply',
+      url: '#',
+    },
+    content:
+      '<ul><li><p>Learn how to foster pets with Austin Animal Center.</p></li><li><p>Learn what types of animals need fostering.</p></li><li><p>Verify your current pets’ rabies vaccinations with your veterinarian.</p></li></ul>',
+  },
+];
