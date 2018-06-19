cd8bb950c120a37c29f163b3ca228134c81e35b6
Simi <simi.damani@austintexas.gov>
2018-06-19 01:48:21 -0500

    add process overview page

--- src/components/Pages/Process.js
+++ src/components/Pages/Process.js
@@ -1,21 +1,72 @@
 import React from 'react';
 import { withRouteData } from 'react-static';
 import { injectIntl } from 'react-intl';
+import path from 'path';
 
-import { processes as i18n } from 'js/i18n/definitions';
-
+import PageBanner from 'components/PageBanner';
+import PageBreadcrumbs from 'components/PageBreadcrumbs';
 import PageHeader from 'components/PageHeader';
-import TileGroup from 'components/Tiles/TileGroup';
+import BadgeGroup from 'components/Badges/BadgeGroup';
+import SectionHeader from 'components/SectionHeader';
+import StepDetailGroup from 'components/Steps/StepDetailGroup';
+import ListSVG from 'components/SVGs/List';
 
-const Process = ({ process: { text: title, processSteps }, intl }) => (
-  <div>
-    <div className="wrapper wrapper--sm container-fluid">
-      <PageHeader title={title} />
+const Process = ({
+  process: {
+    title,
+    text,
+    url,
+    image,
+    topic,
+    topic: { theme },
+    description,
+    processSteps,
+  },
+  intl,
+}) => {
+  const badges = processSteps.map(({ shortTitle, url, sortOrder }) => ({
+    text: shortTitle,
+    url: url,
+    symbol: sortOrder,
+  }));
+  badges.unshift({ text: 'Overview', url: url, isActive: true });
+  const stepDetailGroup = processSteps.map(
+    ({ title, sortOrder, linkTitle, url, overviewSteps }) => ({
+      title: `${sortOrder}. ${title}`,
+      link: { text: linkTitle, url: url },
+      content: overviewSteps,
+    }),
+  );
+  return (
+    <div>
+      <PageBanner
+        imagesPath={`${process.env.CMS_MEDIA}/images`}
+        imageFilename={path.basename(
+          image.filename,
+          path.extname(image.filename),
+        )}
+        imageExtension={path.extname(image.filename)}
+        imageTitle={image.title}
+      />
+      <PageBreadcrumbs
+        grandparent={{ ...theme, subpath: 'themes' }}
+        parent={{ ...topic, subpath: 'topics' }}
+        title={title}
+      />
+      <div className="wrapper wrapper--sm container-fluid">
+        <PageHeader title={title} />
+      </div>
+      <div className="wrapper container-fluid">
+        <BadgeGroup badges={badges} hasBorder={true} />
+      </div>
+      <div className="wrapper wrapper--sm wrapper--hasBorder container-fluid">
+        <SectionHeader description={description} symbol={<ListSVG />}>
+          Overview
+        </SectionHeader>
+      </div>
+      <StepDetailGroup steps={stepDetailGroup} stepClassName="wrapper--sm" />
     </div>
-    <div className="wrapper container-fluid">
-      <TileGroup tiles={processSteps} tag={intl.formatMessage(i18n.process)} />
-    </div>
-  </div>
-);
+  );
+};
 
 export default withRouteData(injectIntl(Process));
