a0ea6db68c8f8ab35d93da571ece006f5b15fff3
Simi <simi.damani@austintexas.gov>
2018-07-10 13:59:39 -0500

    use component did mount for data fetch skeleton

--- src/components/CMSPreview.js
+++ src/components/CMSPreview.js
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl } from 'react-intl';
-const CMSPreview = ({
-  intl,
-  match: {
-    params: { revision_id, page_type },
-  },
-}) => {
-  var toRender;
-  switch (page_type) {
-    case 'services':
-      toRender = `<h1>Service - ${revision_id}</h1>`;
-      break;
-    case 'processes':
-      toRender = `<h1>Process - ${revision_id}</h1>`;
-      break;
-    default:
-      toRender = `<h1>OH NO - ${revision_id}</h1>`;
-      break;
+class CMSPreview extends Component {
+  constructor(props) {
+    super(props);
+
+    const {
+      intl,
+      match: {
+        params: { revision_id, page_type },
+      },
+    } = props;
+
+    this.state = {
+      content: null,
+      component: null,
+      revision_id: revision_id,
+      page_type: page_type,
+    };
   }
-  return toRender;
-};
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    //fetch data based on revision id and page type
+    // switch (this.state.page_type) {
+    //   case 'services':
+    //     toRender = `<h1>ServicePage - ${this.state.content.title}</h1>`;
+    //     break;
+    //   case 'processes':
+    //     toRender = `<h1>ProcessPage - ${this.state.content.title}</h1>`;
+    //     break;
+    //   default:
+    //     toRender = `<h1>OH NO - ${this.state.content.title}</h1>`;
+    //     break;
+    // }
+    this.setState({
+      content: {
+        title: `${this.state.page_type} - ${this.state.revision_id}`,
+      },
+    });
+  }
+
+  render() {
+    if (!this.state.content) return null;
+    const { title } = this.state.content;
+    return `<h1>${title}</h1>`;
+  }
+}
 
 CMSPreview.propTypes = {};
 
