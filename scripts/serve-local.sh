41196263bcccddefe44a3098b8808e24e420265e
Sergio <sergiogcx@gmail.com>
2018-10-19 16:46:33 -0500

    Added support for Linux local development & overriding Joplin host endpoint

--- scripts/serve-local.sh
+++ scripts/serve-local.sh
@@ -7,7 +7,26 @@ TAG='janis:local'
 echo "building docker image..."
 docker build --tag "$TAG" .
 echo "running docker image..."
-HOST_IP=$(ifconfig en0 | awk '$1 == "inet" {print $2}')
+
+# Determine local machine IP (OSX & Linux only)
+case "$OSTYPE" in
+  darwin*)  HOST_IP=$(ifconfig en0 | awk '$1 == "inet" {print $2}');;
+  *)        HOST_IP=$(hostname  -I | awk '{ print '$1' }');;
+esac
+
+# Allow overriding the host we want to use, use IP if none specified.
+if [ "${HOST}" != "" ]; then
+  FINAL_HOST=$HOST
+else
+  FINAL_HOST="${HOST_IP}:8000"
+fi;
+
+# Output to console for debugging
+echo -ne "\e[33m\e[1m"
+echo -e "> Using Host:       ${FINAL_HOST}"
+echo -e "        CMS_API:    http://${FINAL_HOST}/api/graphql"
+echo -e "        CMS_MEDIA:  http://${FINAL_HOST}/media"
+echo -ne "\e[0m"
 
 docker run \
     --rm \
@@ -22,6 +41,6 @@ docker run \
     --volume "$PWD/.babelrc:/app/.babelrc" \
     --env "GOOGLE_ANALYTICS=UA-110716917-2" \
     --env "FEEDBACK_API=https://coa-test-form-api.herokuapp.com/process/" \
-    --env "CMS_API=http://$HOST_IP:8000/api/graphql" \
-    --env "CMS_MEDIA=http://$HOST_IP:8000/media" \
+    --env "CMS_API=http://${FINAL_HOST}/api/graphql" \
+    --env "CMS_MEDIA=http://${FINAL_HOST}/media" \
     "$TAG" "$@"
