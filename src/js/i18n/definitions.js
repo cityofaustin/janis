1613e230b1b844411696b017c453dcc896eec387
Gonzalo Aragon <gonzalo@ranklab.com>
2019-03-08 15:33:55 -0600

    updates check out related services copy

--- src/js/i18n/definitions.js
+++ src/js/i18n/definitions.js
@@ -49,8 +49,10 @@ export const formFeedback = defineMessages({
 export const misc = defineMessages({
   airport: 'Airport',
   citySeal: 'City of Austin Seal',
-  workInProgress: '{alphaSiteLink} is a new website and a work in progress. For the full City of Austin website, visit {citySiteLink}. Learn more about the new website at {projectsSiteLink}.',
-  workInProgressClipped: '{alphaSiteLink} is a work in progress.\nLearn more on our {projectsSiteLink}',
+  workInProgress:
+    '{alphaSiteLink} is a new website and a work in progress. For the full City of Austin website, visit {citySiteLink}. Learn more about the new website at {projectsSiteLink}.',
+  workInProgressClipped:
+    '{alphaSiteLink} is a work in progress.\nLearn more on our {projectsSiteLink}',
   forFullVisit: 'For the full City of Austin website, visit ',
   overview: 'Overview',
   privacy: 'Read About Privacy',
@@ -61,9 +63,9 @@ export const misc = defineMessages({
   opoDeptUrl: 'https://alpha.austin.gov/police-oversight',
   coaOfficialWeb: 'An official website of the City of Austin',
   officialHowYouKnow: 'Here’s how you know.',
-  services: "Services",
-  info: "Information",
-  projectsSiteLinkText: "project site."
+  services: 'Services',
+  info: 'Information',
+  projectsSiteLinkText: 'project site.',
 });
 
 export const navigation = defineMessages({
@@ -80,7 +82,7 @@ export const processes = defineMessages({
 });
 
 export const services = defineMessages({
-  checkOutRelatedServices: 'Check out related services',
+  checkOutRelatedServices: 'Related services',
   checkOutServices: 'Check out City of Austin Services',
   service: 'Service',
 });
@@ -110,28 +112,32 @@ export const curbsideServices = defineMessages({
 });
 
 export const departmentPage = defineMessages({
-  complaintFormButtonText: "File a complaint against an Austin Police Officer",
-  complaintFormUrl: "https://forms.austin.gov/police-oversight/complaint",
-  thankFormButtonText: "Thank the Austin Police Department",
-  thankFormButtonUrl: "https://forms.austin.gov/police-oversight/complaint",
-  thankFormButtonText: "Thank an Austin Police Officer",
-  thankFormUrl: "https://forms.austin.gov/police-oversight/thank",
-  yourDataLinkText: "How we store and use your data",
-  yourDataUrl: "http://alpha.austin.gov/police-oversight/how-we-store-and-use-your-data",
-  investigationProcessLinkText: "Complaint Investigation Process",
-  investigationProcessUrl: "http://alpha.austin.gov/police-oversight/complaint-investigation-process",
-  directorTitle: "Director, Office of Police Oversight",
-  coa: "City of Austin",
-  mission: "Our mission",
-  whatWeDo: "What we do",
-  meetDirector: "Meet our director",
-  meetDirectors: "Meet our directors",
-  topServices: "Top services"
+  complaintFormButtonText: 'File a complaint against an Austin Police Officer',
+  complaintFormUrl: 'https://forms.austin.gov/police-oversight/complaint',
+  thankFormButtonText: 'Thank the Austin Police Department',
+  thankFormButtonUrl: 'https://forms.austin.gov/police-oversight/complaint',
+  thankFormButtonText: 'Thank an Austin Police Officer',
+  thankFormUrl: 'https://forms.austin.gov/police-oversight/thank',
+  yourDataLinkText: 'How we store and use your data',
+  yourDataUrl:
+    'http://alpha.austin.gov/police-oversight/how-we-store-and-use-your-data',
+  investigationProcessLinkText: 'Complaint Investigation Process',
+  investigationProcessUrl:
+    'http://alpha.austin.gov/police-oversight/complaint-investigation-process',
+  directorTitle: 'Director, Office of Police Oversight',
+  coa: 'City of Austin',
+  mission: 'Our mission',
+  whatWeDo: 'What we do',
+  meetDirector: 'Meet our director',
+  meetDirectors: 'Meet our directors',
+  topServices: 'Top services',
 });
 
 export const howYouKnowMenu = defineMessages({
-  dotGovHeader: "The .gov means it’s official.",
-  dotGovText: "Government websites often end in .gov. Before sharing sensitive information, make sure you’re on a federal government site.",
-  httpsHeader: "The site is secure.",
-  httpsText: "The https:// ensures that you are connecting to the official website and that any information you provide is encrypted and transmitted securely."
+  dotGovHeader: 'The .gov means it’s official.',
+  dotGovText:
+    'Government websites often end in .gov. Before sharing sensitive information, make sure you’re on a federal government site.',
+  httpsHeader: 'The site is secure.',
+  httpsText:
+    'The https:// ensures that you are connecting to the official website and that any information you provide is encrypted and transmitted securely.',
 });
