2a4e5c49666a850a1bf031c0e37c75fa2bedea04
Gonzalo Aragon <gonzalo@ranklab.com>
2019-03-08 11:40:55 -0600

    removes SectionHeader from HtmlFromAdmin, not being used

--- src/components/HtmlFromAdmin/index.js
+++ src/components/HtmlFromAdmin/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import SectionHeader from 'components/SectionHeader';
-
 const HtmlFromAdmin = ({ title, content }) => (
   <div className="coa-HtmlFromAdmin">
-    {/* {title && <SectionHeader>{title}</SectionHeader>} */}
     <div
       className="coa-HtmlFromAdmin__content"
       dangerouslySetInnerHTML={{ __html: content }}
