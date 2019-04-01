0e6afcf598ecc96de3c6d8f014f915d9c8567d82
Sergio <sergiogcx@gmail.com>
2019-04-01 12:00:27 -0500

    Updated static.config.js

--- static.config.js
+++ static.config.js
@@ -181,7 +181,8 @@ const makeDepartmentPages = async client => {
 };
 
 export default {
-  // siteRoot: 'https://alpha.austin.gov',
+  //siteRoot: 'https://alpha.austin.gov',
+  //siteRoot // Do not alter this line if you want a working PR
   //basePath // Do not alter this line if you want a working PR
   stagingSiteRoot: 'https://janis-staging.herokuapp.com/',
   getSiteProps: () => ({
