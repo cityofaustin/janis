c8de3f1080cf66c41b942c5bb3b6ecac47c2f190
Brian Smith <briaguya@gmail.com>
2019-04-18 00:09:59 -0500

    clean up process page queries

--- src/js/queries/getProcessPageRevisionQuery.js
+++ src/js/queries/getProcessPageRevisionQuery.js
@@ -1,44 +1,15 @@
+import processPageFragment from './processPageFragment';
+
 const getProcessPageRevisionQuery = `
   query getPageRevision($id: ID) {
     pageRevision(id: $id) {
       id
       asProcessPage {
-        title
-        description
-        slug
-        topic {
-          slug
-          text
-          theme {
-            slug
-            text
-          }
-        }
-        image {
-          filename
-          title
-        }
-        processSteps {
-          edges {
-            node {
-              sortOrder
-              title
-              shortTitle
-              linkTitle
-              description
-              overviewSteps
-              detailedContent
-              quote
-              image {
-                filename
-                title
-              }
-            }
-          }
-        }
+        ...processPageInfo
       }
     }
   }
+  ${processPageFragment}
 `;
 
 export default getProcessPageRevisionQuery;
