e58667499b681e419ef59add99276d497ec57990
Gonzalo Aragon <garagon@gmail.com>
2019-04-24 14:41:04 -0500

    adds title to TC

--- src/components/Pages/TopicCollection.js
+++ src/components/Pages/TopicCollection.js
@@ -7,10 +7,12 @@ import { topics as i18n } from 'js/i18n/definitions';
 import PageHeader from 'components/PageHeader';
 import TileGroup from 'components/Tiles/TileGroup';
 
-const TopicCollection = ({ topics, intl }) => (
+const TopicCollection = ({ topics, intl, tc: { title, description } }) => (
   <div>
-    <div className="wrapper wrapper--sm container-fluid">
-      <PageHeader>TOPIC COLLECTION</PageHeader>
+    <div className="wrapper container-fluid">
+      {/* <PageHeader>TOPIC COLLECTION</PageHeader> */}
+      <PageHeader>{title}</PageHeader>
+      {description}
     </div>
     <div className="wrapper container-fluid">
       {/*<TileGroup tiles={topics} tag={intl.formatMessage(i18n.topic)} />*/}
