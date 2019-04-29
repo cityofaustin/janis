c1349974efea97e966d08bdb13fd60b58388fa48
Gonzalo Aragon <garagon@gmail.com>
2019-04-29 15:55:39 -0500

    implementing TC design, WIP

--- src/components/Pages/TopicCollection.js
+++ src/components/Pages/TopicCollection.js
@@ -6,49 +6,99 @@ import { topics as i18n } from 'js/i18n/definitions';
 
 import PageHeader from 'components/PageHeader';
 import TileGroup from 'components/Tiles/TileGroup';
+import Tile from 'components/Tiles/Tile';
 import ContextualNav from '../PageSections/ContextualNav';
 
-const TopicCollection = ({ intl, tc: { title, description, topics } }) => (
+const TopicCollection = ({
+  intl,
+  tc: { title, description, theme, topics, slug },
+}) => (
   <div>
-    <div className="wrapper container-fluid">
-      {/* <ContextualNav
-        topic={topic}
-        topiccollection={topic && topic.topiccollection}
-        theme={theme}
-      /> */}
+    {/*
 
-      <PageHeader>{title}</PageHeader>
-      {description && <p>{description}</p>}
-    </div>
-    <div className="wrapper container-fluid">
-      {/*<TileGroup tiles={topics} tag={intl.formatMessage(i18n.topic)} />*/}
-    </div>
-    {topics.map((topic, index) => {
-      const pages =
-        topic.topLinks && topic.topLinks.length
-          ? topic.toplinks
-          : topic.otherLinks && topic.otherLinks.length
-          ? topic.otherLinks.slice(0, 4)
-          : null;
+    TopicCollection can belong to other TopicCollection or Theme.
+    Add placeholder related to
 
-      return (
-        <div key={index}>
-          <h2>{topic.title}</h2>
-          {topic.description && <p>{topic.description}</p>}
-          {pages
-            ? pages.map((link, index) => (
-                <ul>
-                  <li key={index}>
-                    <div>Title: {link.title}</div>
-                    <div>Type: {link.type}</div>
-                    <div>URL: {link.url}</div>
-                  </li>
-                </ul>
-              ))
-            : null}
+    <ContextualNav
+      topic={topic}
+      topiccollection={topic && topic.topiccollection}
+      theme={theme}
+    /> */}
+
+    <PageHeader description={description} contentType={'topic-collection'}>
+      {title}
+    </PageHeader>
+
+    <div className="wrapper container-fluid">
+      <div className="row">
+        <div className="col-xs-12">
+          <div className="coa-TopicCollectionCard__container">
+            {topics.map((topic, index) => {
+              const pages =
+                topic.topLinks && topic.topLinks.length
+                  ? topic.toplinks
+                  : topic.otherLinks && topic.otherLinks.length
+                    ? topic.otherLinks.slice(0, 4)
+                    : null;
+
+              const servicePages = pages.filter(
+                page => page.type === 'service',
+              );
+              const infoPages = pages.filter(page => page.type === 'info');
+
+              return (
+                <div key={index} className="coa-TopicCollectionCard">
+                  <h3 className="coa-TopicCollectionCard__title">
+                    <a href={`/${theme.slug}/${slug}/${topic.slug}`}>
+                      {topic.title}
+                    </a>
+                    <i className="material-icons">arrow_right_alt</i>
+                  </h3>
+                  {topic.description && (
+                    <p className="coa-TopicCollectionCard__description">
+                      {topic.description}
+                    </p>
+                  )}
+                  {pages ? (
+                    <ul className="coa-TopicCollectionCard__links">
+                      {servicePages &&
+                        servicePages.map((link, index) => (
+                          <li
+                            className="coa-TopicCollectionCard__item"
+                            key={index}
+                          >
+                            <Tile text={link.title} url={link.url} />
+                          </li>
+                        ))}
+                      {infoPages &&
+                        infoPages.map((link, index) => (
+                          <li
+                            className="coa-TopicCollectionCard__item"
+                            key={index}
+                          >
+                            <a
+                              className="coa-TopicCollectionCard__info-link"
+                              href={link.url}
+                            >
+                              {link.title}
+                            </a>
+                          </li>
+                        ))}
+                    </ul>
+                  ) : null}
+                  <a
+                    href={`/${theme.slug}/${slug}/${topic.slug}`}
+                    className="coa-TopicCollectionCard__link"
+                  >
+                    Learn More
+                  </a>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      );
-    })}
+      </div>
+    </div>
   </div>
 );
 
