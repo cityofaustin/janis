200ce9095735014c47b50511e4f20292be3cfcd5
Brian Smith <briaguya@gmail.com>
2019-05-08 17:08:18 -0500

    create react static 7 app

---
+++ janis/artifacts/react-static-templates.js
@@ -0,0 +1,14 @@
+
+
+  
+// Template Map
+export default {
+  '../src/pages/404.js': require('../src/pages/404.js').default,
+'../src/pages/about.js': require('../src/pages/about.js').default,
+'../src/pages/blog.js': require('../src/pages/blog.js').default,
+'../src/pages/index.js': require('../src/pages/index.js').default,
+'../src/containers/Post': require('../src/containers/Post').default
+}
+
+export const notFoundTemplate = '../src/pages/404.js'
+
