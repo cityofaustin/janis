390a173bbc73f88c6886cedff3aa8c78c60f2a75
Eric Sherman <ericandrewsherman@gmail.com>
2019-05-14 14:12:56 -0500

    tweaks for mobile

--- src/components/HeroHome/index.js
+++ src/components/HeroHome/index.js
@@ -12,7 +12,12 @@ const HeroHome = ({ imageFilename, imageTitle, preheader }) => (
       widths={FULL_WIDTH_RESPONSIVE_IMAGE_SIZES}
       altText={imageTitle}
     />
-    <div className="coa-HeroHome" role="img" aria-label={imageTitle}>
+    <div
+      className="coa-HeroHome"
+      role="img"
+      aria-label={imageTitle}
+      style={{ zIndex: `2`, position: `relative` }}
+    >
       <div className="container-fluid wrapper">
         <span className="coa-HeroHome__preheader">{preheader}</span>
         <h2 className="coa-HeroHome__header">Austin, Texas</h2>
