9c97b46d3f10b98600a74b933ee9b3494881ff86
Sergio <sergiogcx@gmail.com>
2019-05-14 14:39:10 -0500

    Removed cache clear method, added unique id to slack messages

--- worker/deploy.sh
+++ worker/deploy.sh
@@ -9,10 +9,17 @@ source /app/aws-helper.sh;
 
 #
 # Now we need the file name and file url
-#
-FILE_UUID="$(cat /proc/sys/kernel/random/uuid | sha256sum | cut -d ' ' -f 1).txt"
-LOG_URL="https://s3.amazonaws.com/${AWS_BUCKET_NAME}/_reserved/logs/${FILE_UUID}"
 
+# Let's generate a unique hash
+UNIQUE_HASH=$(cat /proc/sys/kernel/random/uuid | sha256sum | cut -d ' ' -f 1);
+# This is the unique log file name
+FILE_UUID="${UNIQUE_HASH}.txt"
+# This is the build log link that will be published in slack
+LOG_URL="https://s3.amazonaws.com/${AWS_BUCKET_NAME}/_reserved/logs/${FILE_UUID}"
+# We need this to identify builds in slack messages
+BUILD_ID=$(echo -n "${UNIQUE_HASH}" | cut -c1-6);
+# For our time elapsed method
+SECONDS=0
 
 #
 # Let's now notify the channel a new request
@@ -25,13 +32,14 @@ if [[ "$SLACK_URL" != "" ]]; then
   #
   if [[ "${SLACK_MESSAGE}" != "" ]]; then
       curl -X POST -H 'Content-type: application/json' \
-            --data "{\"text\":\"${SLACK_MESSAGE}\"}" \
+            --data "{\"text\":\"$(date) ${BUILD_ID} -- ${SLACK_MESSAGE}\"}" \
             $SLACK_URL;
-  else
-    curl -X POST -H 'Content-type: application/json' \
-    --data "{\"text\":\":coffee: We've received a request to build janis. Take a cup of coffee, you will be notified whenever it's done.\"}" \
-    $SLACK_URL;
   fi;
+
+  # Now we signal that the request to build janis has been received.
+  curl -X POST -H 'Content-type: application/json' \
+    --data "{\"text\":\"$(date) ${BUILD_ID} -- :coffee: We've received a request to build janis. Take a cup of coffee, you will be notified whenever it's done.\"}" \
+    $SLACK_URL;
 fi;
 
 function upload_logfile {
@@ -44,7 +52,7 @@ function error_message_slack {
     upload_logfile;
 
     curl -X POST -H 'Content-type: application/json' \
-    --data "{\"text\":\":x: There was a problem building janis. Check the logs for details. ${LOG_URL}\"}" \
+    --data "{\"text\":\"$(date) ${BUILD_ID} -- :x: There was a problem building janis. Check the logs for details. ${LOG_URL}\"}" \
     $SLACK_URL;
   fi;
 }
@@ -81,6 +89,13 @@ fi;
 #
 janis_deploy |& tee -a $FILE_UUID;
 
+
+
+#
+# We now have to clear the cache (if a distro is defined)
+#
+janis_clearcache |& tee -a $FILE_UUID;
+
 echo "Last Exit Code: $?"
 
 #
@@ -110,17 +125,11 @@ echo "LOG_URL: ${LOG_URL}";
 # We are going to submit a notification to slack when complete.
 #
 if [[ "$SLACK_URL" != "" ]]; then
+  duration=$SECONDS;
+
   curl -X POST -H 'Content-type: application/json' \
-        --data "{\"text\":\":white_check_mark: ${BUILD_MESSAGE} -- ${LOG_URL}\"}" \
+        --data "{\"text\":\"$(date) ${BUILD_ID} -- :white_check_mark: ${BUILD_MESSAGE} -- ${LOG_URL}\"} -- $(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed." \
         $SLACK_URL;
 fi;
 
-#
-# Now we need to clear the cache.
-#
-if [[ "${CF_DISTRO}" != "" ]]; then
-  echo "Clearing cache for distribution: ${CF_DISTRO}";
-  aws cloudfront create-invalidation --distribution-id $CF_DISTRO --paths "/*";
-  echo "Invalidation request submitted!";
-fi;
 
