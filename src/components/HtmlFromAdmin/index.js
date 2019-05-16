ed014730d45bc81a112309bce4784b4441964d32
Brian Smith <briaguya@gmail.com>
2019-05-16 07:04:00 -0500

    hack some stuff together using markdown parsing

--- src/components/HtmlFromAdmin/index.js
+++ src/components/HtmlFromAdmin/index.js
@@ -1,8 +1,48 @@
 import React from 'react';
 import Parser from 'html-react-parser';
+import ReactMarkdown from 'react-markdown';
 import PropTypes from 'prop-types';
 
-const HtmlFromAdmin = ({ content }) => Parser(content);
+const HtmlFromAdmin = ({ content }) => {
+  // Really hacking it together here
+  const domParser = new DOMParser();
+  const doc = domParser.parseFromString(content, 'text/html');
+  var kids = [...doc.body.children];
+  var useMarkdown = false;
+  var markdown = '';
+
+  for(const kid of kids) {
+    if(kid.lastChild.localName === 'code') {
+      useMarkdown = true;
+      markdown = markdown.concat(kid.innerText, '\n');
+
+    } else {
+      markdown = markdown.concat(kid.outerHTML, '\n\n');
+    }
+  }
+
+  if(useMarkdown) {
+    return <ReactMarkdown source={markdown} escapeHtml={false}/>;
+  } else {
+    return Parser(content);
+  }
+
+  return null;
+
+  // for (var index = 0; index < kids.length; index++) {
+  //   if (kids[index].lastChild.localName === 'code') {
+  //     indexesToReplace.push(index);
+  //     tableMarkdown = tableMarkdown.concat(kids[index].innerText, '\n');
+  //   }
+  // }
+
+  // console.log(indexesToReplace);
+  // console.log(tableMarkdown);
+
+  // const stringy = kids.map(k => k.outerHTML).join('');
+
+  // return Parser(stringy);
+};
 
 HtmlFromAdmin.propTypes = {
   content: PropTypes.string.isRequired,
