cb2c0e1d15e20c3f1f686432cf4a30d22149c98b
Brian Smith <briaguya@gmail.com>
2019-05-16 08:08:06 -0500

    hardcode to my joplin pr app

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,14 +3,15 @@
 set -o errexit
 
 TAG=${TAG:-janis-build:local}
-HOST_IP=$(ifconfig en0 | awk '$1 == "inet" {print $2}')
-CMS_URL=${CMS_URL:-http://$HOST_IP:8000}
+# HOST_IP=$(ifconfig en0 | awk '$1 == "inet" {print $2}')
+# CMS_URL=${CMS_URL:-http://$HOST_IP:8000}
+CMS_URL=https://joplin-staging-pr-234.herokuapp.com
 
 echo "Building image..."
 docker build \
     --rm \
     --build-arg "CMS_API=$CMS_URL/api/graphql" \
-    --build-arg "CMS_MEDIA=$CMS_URL/media" \
+    --build-arg "CMS_MEDIA=https://joplin-austin-gov.s3.amazonaws.com/media" \
     --build-arg "FEEDBACK_API=https://coa-test-form-api.herokuapp.com/process/" \
     --build-arg "GOOGLE_ANALYTICS=UA-110716917-2" \
     --tag "$TAG" \
