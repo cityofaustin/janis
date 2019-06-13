0264895af25248f716d00c55fc32b7c1d39c397b
Eric Sherman <ericandrewsherman@gmail.com>
2019-06-13 17:11:28 -0500

    dumb example from formik

--- src/components/Pages/Form.js
+++ src/components/Pages/Form.js
@@ -1,5 +1,34 @@
 import React, { Component } from 'react';
 import { withRouteData } from 'react-static';
+import { Formik } from 'formik';
+
+const BasicExample = () => (
+  <div>
+    <h1>My Form</h1>
+    <Formik
+      initialValues={{ name: 'jared' }}
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          actions.setSubmitting(false);
+        }, 1000);
+      }}
+      render={props => (
+        <form onSubmit={props.handleSubmit}>
+          <input
+            type="text"
+            onChange={props.handleChange}
+            onBlur={props.handleBlur}
+            value={props.values.name}
+            name="name"
+          />
+          {props.errors.name && <div id="feedback">{props.errors.name}</div>}
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    />
+  </div>
+);
 
 const Form = props => {
   console.log(props.form);
@@ -18,6 +47,7 @@ const Form = props => {
         {formFields}
         <input type="submit" />
       </form>
+      <BasicExample />
     </section>
   );
 };
