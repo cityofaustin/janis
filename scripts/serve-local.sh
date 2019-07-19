1f9ef8b5a5f878588427d0da48efccccc16b083c
Brian Smith <briaguya@gmail.com>
2019-07-19 18:19:00 -0500

    revert serve local

--- scripts/serve-local.sh
+++ scripts/serve-local.sh
@@ -47,7 +47,7 @@ fi
 while getopts "PSe:" opt; do
   case $opt in
     P )
-      CMS_API="https://joplin-staging-pr-234.herokuapp.com/api/graphql"
+      CMS_API="https://joplin.herokuapp.com/api/graphql"
       CMS_MEDIA="https://joplin-austin-gov.s3.amazonaws.com/media"
       ;;
     S)
