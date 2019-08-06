69d0eb4e17f201f43de7eaba304a0753268d4b30
Niicck <niicck@users.noreply.github.com>
2019-08-06 18:05:17 -0500

    Issue 2688: add pdf size for official documents

--- src/components/Pages/OfficialDocument.js
+++ src/components/Pages/OfficialDocument.js
@@ -1,23 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl } from 'react-intl';
 import moment from 'moment-timezone';
+import axios from 'axios';
+import prettyBytes from 'pretty-bytes';
 
-const OfficialDocument = ({ document: { id, date, title, authoringOffice, summary, name, link }, intl }) => (
-  <div className="coa-OfficialDocumentPage__container">
-    <div className="coa-OfficialDocumentPage__date">
-      {moment(date, "YYYY-MM-DD").format('LL')}
-    </div>
-    <h2 className="coa-OfficialDocumentPage__title">{title}</h2>
-    <p>{summary}</p>
-    <div className="coa-OfficialDocumentPage__small-heading-container">
-      <span className="coa-OfficialDocumentPage__small-heading">Author:</span> {authoringOffice}
-    </div>
-    <div className="coa-OfficialDocumentPage__small-heading-container">
-      <span className="coa-OfficialDocumentPage__small-heading">Document:</span> <a href={link}>{name}</a>
-    </div>
-  </div>
-);
+class OfficialDocument extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pdfSize: null
+    }
+  }
+
+  componentDidMount() {
+    // Get the size of attached pdf link.
+    // This is an optional UI addition. If the link is not a PDF, no need to throw an error.
+    axios({
+      method: 'HEAD',
+      url: this.props.document.link,
+      headers: {
+        Origin: "alpha.austin.gov"
+      }
+    })
+    .then(res => {
+      if (res.headers["content-type"] === "application/pdf") {
+        this.setState({pdfSize: prettyBytes(+res.headers["content-length"]).replace(" ", "")});
+      }
+    })
+    .catch(error => null)
+  }
+
+  render() {
+    const { id, date, title, authoringOffice, summary, name, link } = this.props.document;
+    const { pdfSize } = this.state;
+
+    let pdfSizeComponent = null;
+    if (pdfSize) {
+      pdfSizeComponent = <span className="coa-OfficialDocumentPage__pdf-size">(PDF {pdfSize})</span>
+    }
+
+    return (
+      <div className="coa-OfficialDocumentPage__container">
+        <div className="coa-OfficialDocumentPage__date">
+          {moment(date, "YYYY-MM-DD").format('LL')}
+        </div>
+        <h2 className="coa-OfficialDocumentPage__title">{title}</h2>
+        <p>{summary}</p>
+        <div className="coa-OfficialDocumentPage__small-heading-container">
+          <span className="coa-OfficialDocumentPage__small-heading">Author:</span> {authoringOffice}
+        </div>
+        <div className="coa-OfficialDocumentPage__small-heading-container">
+          <span className="coa-OfficialDocumentPage__small-heading">Document:</span> <a href={link}>{name}</a> {pdfSizeComponent}
+        </div>
+      </div>
+    )
+  }
+};
 
 OfficialDocument.propTypes = {
   document: PropTypes.shape({
@@ -28,8 +67,7 @@ OfficialDocument.propTypes = {
     summary: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     link: PropTypes.string.isRequired,
-  }),
-  bottomBorder: PropTypes.bool.isRequired,
+  })
 }
 
 export default injectIntl(OfficialDocument);
