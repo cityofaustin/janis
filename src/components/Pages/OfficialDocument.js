0e4b4b65c1de0b9aaa283bc25def13206a55c0db
Niicck <niicck@users.noreply.github.com>
2019-08-07 14:37:50 -0500

    Issue 2688: add PDF styling for official documents

--- src/components/Pages/OfficialDocument.js
+++ src/components/Pages/OfficialDocument.js
@@ -3,21 +3,28 @@ import PropTypes from 'prop-types';
 import { injectIntl } from 'react-intl';
 import moment from 'moment-timezone';
 
-const OfficialDocument = ({ document: { id, date, title, authoringOffice, summary, name, link }, intl }) => (
-  <div className="coa-OfficialDocumentPage__container">
-    <div className="coa-OfficialDocumentPage__date">
-      {moment(date, "YYYY-MM-DD").format('LL')}
+const OfficialDocument = ({ document: { id, date, title, authoringOffice, summary, name, link }, intl }) => {
+  let pdfComponent = null;
+  if (!!link.match(/\.pdf$/)) {
+    pdfComponent = <span className="coa-OfficialDocumentPage__pdf">(PDF)</span>
+  }
+
+  return (
+    <div className="coa-OfficialDocumentPage__container">
+      <div className="coa-OfficialDocumentPage__date">
+        {moment(date, "YYYY-MM-DD").format('LL')}
+      </div>
+      <h2 className="coa-OfficialDocumentPage__title">{title}</h2>
+      <p>{summary}</p>
+      <div className="coa-OfficialDocumentPage__small-heading-container">
+        <span className="coa-OfficialDocumentPage__small-heading">Author:</span> {authoringOffice}
+      </div>
+      <div className="coa-OfficialDocumentPage__small-heading-container">
+        <span className="coa-OfficialDocumentPage__small-heading">Document:</span> <a href={link}>{name}</a> {pdfComponent}
+      </div>
     </div>
-    <h2 className="coa-OfficialDocumentPage__title">{title}</h2>
-    <p>{summary}</p>
-    <div className="coa-OfficialDocumentPage__small-heading-container">
-      <span className="coa-OfficialDocumentPage__small-heading">Author:</span> {authoringOffice}
-    </div>
-    <div className="coa-OfficialDocumentPage__small-heading-container">
-      <span className="coa-OfficialDocumentPage__small-heading">Document:</span> <a href={link}>{name}</a>
-    </div>
-  </div>
-);
+  );
+}
 
 OfficialDocument.propTypes = {
   document: PropTypes.shape({
@@ -29,7 +36,6 @@ OfficialDocument.propTypes = {
     name: PropTypes.string.isRequired,
     link: PropTypes.string.isRequired,
   }),
-  bottomBorder: PropTypes.bool.isRequired,
 }
 
 export default injectIntl(OfficialDocument);
