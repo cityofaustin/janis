94510381734c1d078a90523ce6a31d7a0a4fb4d2
Brian Smith <briaguya@gmail.com>
2019-08-06 23:56:21 -0500

    build against staging

--- scripts/build-joplin-cloud.sh
+++ scripts/build-joplin-cloud.sh
@@ -4,8 +4,8 @@ export NODE_PATH='./src'
 export CMS_API='https://joplin.herokuapp.com/api/graphql'
 export CMS_MEDIA='https://joplin-austin-gov.s3.amazonaws.com/media'
 
-if [ "$HEAD" == '2572-official-doc' ]; then
-  export CMS_API='https://joplin-pr-2572-official-doc.herokuapp.com/api/graphql'
+if [ "$HEAD" == '2497-title-for-topic-and-tc' ]; then
+  export CMS_API='https://joplin-staging.herokuapp.com/api/graphql'
 fi
 
 yarn npm-run-all build-css build-js
