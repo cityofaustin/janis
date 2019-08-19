d6faebac46cacb170dcecebd74051c1328da8aa2
Niicck <niicck@users.noreply.github.com>
2019-08-19 11:18:15 -0500

    serve-local points to new media file addresses

--- scripts/undockered.sh
+++ scripts/undockered.sh
@@ -6,7 +6,7 @@ CD=`dirname $BASH_SOURCE`
 # Set Env vars
 NODE_PATH=$CD/../src
 CMS_API='http://localhost:8000/api/graphql' # Local API
-CMS_MEDIA='https://joplin-austin-gov.s3.amazonaws.com/media' # Prod images
+CMS_MEDIA='https://joplin-austin-gov-static.s3.amazonaws.com/production/media' # Prod images
 
 # Build
 yarn install
