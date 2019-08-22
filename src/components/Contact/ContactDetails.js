3b5f7becfa88f48bcc1fcb320460afe940a09458
Eric Sherman <ericandrewsherman@gmail.com>
2019-08-22 18:38:52 -0500

    Revert "Revert "2702 new phone component based on new joplin structure""
    This reverts commit 372107384522d844db71f0e647748dcbf7c711ad.

--- src/components/Contact/ContactDetails.js
+++ src/components/Contact/ContactDetails.js
@@ -5,7 +5,7 @@ import { injectIntl } from 'react-intl';
 import { contact as i18n } from 'js/i18n/definitions';
 
 import SectionHeader from 'components/SectionHeader';
-import Phone from './Phone';
+import Phones from './Phones';
 import Email from './Email';
 import Address from './Address';
 import Hours from './Hours';
@@ -43,22 +43,21 @@ const ContactSocialMediaLink = ({ url }) => {
 };
 
 const ContactDetails = ({
-  contact: { phone, email, location, hours, socialMedia },
+  contact: { phoneNumber, email, location, hours, socialMedia },
   intl,
 }) => (
   <div className="coa-ContactDetails">
     <SectionHeader isSerif={true}>
       {intl.formatMessage(i18n.questionsTitle)}
     </SectionHeader>
-
     {email && <Email email={email} />}
-
     {location && <Address location={location} />}
-
-    {phone && <Phone phone={phone} />}
-
+    {phoneNumber &&
+      !!phoneNumber.edges.length && (
+          phoneNumber.edges.map(phone =>
+          <Phones phone={phone.node} />)
+      )}
     {hours && !!hours.length && <Hours hours={hours} />}
-
     {socialMedia &&
       socialMedia.map(url => <ContactSocialMediaLink url={url.value} />)}
   </div>
