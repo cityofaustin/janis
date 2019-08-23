32bbaaba66ca55586468a24c7cf28c062b691c0c
Niicck <niicck@users.noreply.github.com>
2019-08-23 12:26:54 -0500

    fix socket.io console errors when running webpack-dev-server

--- scripts/undockered.sh
+++ scripts/undockered.sh
@@ -8,9 +8,51 @@ NODE_PATH=$CD/../src
 CMS_API='http://localhost:8000/api/graphql' # Local API
 CMS_MEDIA='https://joplin-austin-gov-static.s3.amazonaws.com/production/media' # Prod images
 
-# Build
-yarn install
-yarn npm-run-all build-css build-js
+# Process Parameters
+# if -P prod flag is used, then point to prod graphql and CMS
+while getopts "PSe:M:A:" opt; do
+  case $opt in
+    P )
+      CMS_API="https://joplin.herokuapp.com/api/graphql"
+      CMS_MEDIA="https://joplin-austin-gov-static.s3.amazonaws.com/production/media"
+      ;;
+    S)
+      CMS_API="https://joplin-staging.herokuapp.com/api/graphql"
+      CMS_MEDIA="https://joplin-austin-gov-static.s3.amazonaws.com/staging/media"
+      ;;
+    e )
+      EXEC=$OPTARG
+      ;;
+    M )
+      if [ "$OPTARG" == "prod" ]; then
+        CMS_MEDIA="https://joplin-austin-gov-static.s3.amazonaws.com/production/media"
+      elif [ "$OPTARG" == "staging" ]; then
+        CMS_MEDIA="https://joplin-austin-gov-static.s3.amazonaws.com/staging/media"
+      else
+        CMS_MEDIA=$OPTARG
+      fi
+      ;;
+    A )
+      if [ "$OPTARG" == "prod" ]; then
+        CMS_API="https://joplin.herokuapp.com/api/graphql"
+      elif [ "$OPTARG" == "staging" ]; then
+        CMS_API="https://joplin-staging.herokuapp.com/api/graphql"
+      else
+        CMS_MEDIA=$OPTARG
+      fi
+      ;;
+    \? )
+      echo "Invalid option: -$OPTARG" >&2
+      exit;
+      ;;
+    : )
+      echo "Invalid option: -$OPTARG requires an argument" 1>&2
+      exit;
+      ;;
+  esac
+done
 
-# Serve
-http-server dist -p 3000
+echo "CMS_API is: $CMS_API"
+echo "CMS_MEDIA is: $CMS_MEDIA"
+
+yarn start
