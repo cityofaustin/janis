940495e93737e59ed932a12d8dc5e162cc7c6c74
Eric Sherman <ericandrewsherman@gmail.com>
2019-08-27 19:19:13 -0500

    add just a comment

--- src/components/Contact/ContactDetails.js
+++ src/components/Contact/ContactDetails.js
@@ -32,6 +32,10 @@ const ContactDetails = ({
     {location && <Address location={location} />}
     {/*phone list should probably be a list for semantics but it affects styling atm */}
     {phoneNumber && <PhonesList phoneNumbers={phoneNumber} />}
+    {/* We want to keep this component easy to read and streamlined, so we are
+      avoiding complicate functions in this return.
+      However, each social media link is it's own contact item with it's own icon,
+      so it makes sense to have the map here*/}
     {hours && !!hours.length && <Hours hours={hours} />}
     {socialMedia && socialMedia.map(url => <SocialMediaLink url={url.value} />)}
   </div>
