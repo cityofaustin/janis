a76b5f6cd0b295f7e9322e7be9d8828af21f4abc
Niicck <niicck@users.noreply.github.com>
2019-08-28 16:01:55 -0500

    fix positioning for TableOfContents

--- src/components/Pages/Guide/GuideSidebarMobile.js
+++ src/components/Pages/Guide/GuideSidebarMobile.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
 import {hyphenate} from "./helpers";
-import { isMobileOrTablet } from 'js/helpers/reactMediaQueries';
 
 import {
   Accordion,
@@ -13,33 +12,20 @@ import {
 } from 'react-accessible-accordion';
 
 function GuideSidebarMobile({ contact, sections, currentSection }) {
-  console.log(`~~~ ist mobile o tablet? ${isMobileOrTablet()}`)
 
   return (
-    <div className="coa-PsychoSidebar__container sticky">
-      <Accordion className={'coa-GuideSidebar__mobile-accordian-container'} allowZeroExpanded={true}>
-        <AccordionItem className={'coa-GuideSidebar__mobile-accordian-item '}>
-          <AccordionItemPanel className={'coa-GuideSidebar__mobile-accordian-panel'}>
-            BWAWHSFSHHAHAAHAHAHAHAHAH
-          </AccordionItemPanel>
-          <AccordionItemHeading className={'coa-GuideSidebar__mobile-accordian-heading'}>
-            <AccordionItemButton className={'coa-GuideSidebar__mobile-accordian-button'}>
-              <h3 className="coa-GuideSidebar__mobile-accordian-title">Table of Contents</h3>
-
-              <AccordionItemState>
-                {({ expanded }) =>
-                  expanded ? (
-                    <i class="material-icons">remove</i>
-                  ) : (
-                    <i class="material-icons">add</i>
-                  )
-                }
-              </AccordionItemState>
-            </AccordionItemButton>
-          </AccordionItemHeading>
-        </AccordionItem>
-      </Accordion>
-    </div>
+    <Accordion className={'coa-GuideSidebar__mobile-accordian-container'} allowZeroExpanded={true}>
+      <AccordionItem className={'coa-GuideSidebar__mobile-accordian-item'}>
+        <AccordionItemPanel className={'coa-GuideSidebar__mobile-accordian-panel'}>
+          BWAWHSFSHHAHAAHAHAHAHAHAH
+        </AccordionItemPanel>
+        <AccordionItemHeading className={'coa-GuideSidebar__mobile-accordian-heading'}>
+          <AccordionItemButton className={'coa-GuideSidebar__mobile-accordian-button'}>
+            Table of Contents
+          </AccordionItemButton>
+        </AccordionItemHeading>
+      </AccordionItem>
+    </Accordion>
   )
 }
 
