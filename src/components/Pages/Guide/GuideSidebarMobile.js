b7aab0a43bcb4f82d61c22cc56bc85fa657a1e39
Niicck <niicck@users.noreply.github.com>
2019-08-29 16:00:50 -0500

    mobile guide menu stacks above footer and other {position: relative} elements

--- src/components/Pages/Guide/GuideSidebarMobile.js
+++ src/components/Pages/Guide/GuideSidebarMobile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import classNames from 'classnames';
 import {hyphenate} from "./helpers";
 
