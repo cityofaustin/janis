5b7510f1222b48097a3f00a9c6788a5bb1f6a41e
Brian Smith <briaguya@gmail.com>
2019-09-05 01:43:21 -0500

    netlify the netlify

--- scripts/build-joplin-cloud.sh
+++ scripts/build-joplin-cloud.sh
@@ -14,8 +14,8 @@ elif [ "$HEAD" == '2783-mobile-guide-nav' ]; then
   export CMS_API='https://joplin-pr-2783-mobile-guide-na.herokuapp.com/api/graphql'
 fi
 
-if [ "$HEAD" == 'department-top-services' ]; then
-  export CMS_API='https://joplin-staging.herokuapp.com/api/graphql'
+if [ "$HEAD" == '2855-official-doc-top-links' ]; then
+  export CMS_API='http://joplin-pr-2855-official-doc-to.herokuapp.com/api/graphql'
 fi
 
 yarn npm-run-all build-css build-js
