3cab2c97f53e9fd889565c92dda4dfca0b8d9fc8
Bob Main <bobmain49@gmail.com>
2019-09-09 16:45:07 -0500

    adding joplin data for deploy

--- scripts/build-joplin-cloud.sh
+++ scripts/build-joplin-cloud.sh
@@ -18,4 +18,8 @@ if [ "$HEAD" == '2666-hacky-janis-step-apps' ]; then
   export CMS_API='https://joplin-staging.herokuapp.com/api/graphql'
 fi
 
+if [ "$HEAD" == '2662-service-step-styling' ]; then
+  export CMS_API='https://joplin-pr-2662-service-step-st.herokuapp.com/api/graphql'
+fi
+
 yarn npm-run-all build-css build-js
