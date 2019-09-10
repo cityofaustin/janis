8a8ee7ba6ce937178b1608e3b0c6ed9c1ee5118f
Eric Sherman <ericandrewsherman@gmail.com>
2019-09-10 14:25:50 -0500

    remove 'phone'

--- src/js/queries/servicePageFragment.js
+++ src/js/queries/servicePageFragment.js
@@ -55,7 +55,6 @@ const servicePageFragment = `
           contact {
             name
             email
-            phone
             ${phoneFragment}
             socialMedia
             hours {
