1a05b560bd5450bf7f1f4c41620d4614fcfb7d57
Brian Smith <briaguya@gmail.com>
2019-09-19 12:29:24 -0500

    really?

--- scripts/build-joplin-cloud.sh
+++ scripts/build-joplin-cloud.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
 export NODE_PATH='./src'
-export CMS_API='https://joplin.herokuapp.com/api/graphql'
+export CMS_API='https://joplin-staging.herokuapp.com/api/graphql'
 export CMS_MEDIA='https://joplin-austin-gov.s3.amazonaws.com/media'
-
-if [ "$HEAD" == '2906-toc-horizontal-rules' ]; then
-  export CMS_API='https://joplin-staging.herokuapp.com/api/graphql'
-fi
 
 yarn npm-run-all build-css build-js
