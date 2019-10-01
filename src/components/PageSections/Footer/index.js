134f9fef82bb23e1c8316974cf601a473cd59472
Brian Smith <briaguya@gmail.com>
2019-10-01 05:09:46 -0500

    remove commented out part

--- src/components/PageSections/Footer/index.js
+++ src/components/PageSections/Footer/index.js
@@ -11,7 +11,6 @@ import WorkInProgress from 'components/WorkInProgress';
 import TwitterSVG from 'components/SVGs/Twitter';
 import FacebookSVG from 'components/SVGs/Facebook';
 import GithubSVG from 'components/SVGs/Github';
-// import CitySealSVG from 'components/SVGs/CitySeal';
 import citySealImg from 'images/coa_seal_color.png';
 
 import ThreeOneOne from 'components/PageSections/ThreeOneOne';
