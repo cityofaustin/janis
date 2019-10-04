7130dd1edf8dd88a7b0a069c5ad77a310bcb36b7
Brian Smith <briaguya@gmail.com>
2019-10-04 02:45:27 -0500

    build an app with test data

--- scripts/build-joplin-cloud.sh
+++ scripts/build-joplin-cloud.sh
@@ -5,8 +5,9 @@ export CMS_API='https://joplin.herokuapp.com/api/graphql'
 export CMS_MEDIA='https://joplin-austin-gov.s3.amazonaws.com/media'
 export CMS_DOCS='multiple'
 
-if [ "$HEAD" == 'figure-out-build-issues' ]; then
-  export CMS_API='https://joplin-staging.herokuapp.com/api/graphql'
+if [ "$HEAD" == '2908-build-against-test-data' ]; then
+  export CMS_API='https://joplin-pr-2908-test-data.herokuapp.com/api/graphql'
+  export CMS_MEDIA='https://joplin-austin-gov-static.s3.amazonaws.com/staging/media'
 fi
 
 yarn npm-run-all build-css build-js
