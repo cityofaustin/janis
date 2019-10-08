7c447cf5c3f79e6fa32679d749bd1067953aad4c
Brian Smith <briaguya@gmail.com>
2019-10-08 05:38:15 -0500

    make prod joplin default for PR apps again

--- scripts/build-joplin-cloud.sh
+++ scripts/build-joplin-cloud.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export NODE_PATH='./src'
-export CMS_API='https://joplin-staging.herokuapp.com/api/graphql'
+export CMS_API='https://joplin.herokuapp.com/api/graphql'
 export CMS_MEDIA='https://joplin-austin-gov.s3.amazonaws.com/media'
 export CMS_DOCS='multiple'
 
