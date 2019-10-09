c4fa7a466bfc8e50f3c58cc2264e6a0a4f52f886
Niicck <niicck@users.noreply.github.com>
2019-10-09 11:06:58 -0500

    simple netlify publish script

---
+++ deploy/prBranch.config.js
@@ -0,0 +1,22 @@
+/**
+  For the initial site build, if you want to pull data from a joplin instance other than staging,
+  plug in the CMS_API you want to use here.
+
+  Note: publish commands from Joplin will re-override the CMS_API value.
+  That means that deploys from subsequent github code pushes will pull data from whichever Joplin last triggered a publish,
+  not the initial value in this file.
+
+  Example:
+  "2997-dept-links": {
+    CMS_API: "https://joplin-pr-2997-dept-links.herokuapp.com/api/graphql"
+  }
+  "2997-dept-links" is the name of the janis branch.
+  "https://joplin-pr-2997-dept-links.herokuapp.com/api/graphql" is the address of the deployed joplin instance you want to query data from.
+  That CMS_API value will only be used by the branch "2997-dept-links".
+**/
+
+module.exports = {
+  "2997-dept-links": {
+    CMS_API: "https://joplin-pr-2997-dept-links.herokuapp.com/api/graphql"
+  },
+}
---
+++ deploy/publish-netlify-pr.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "This triggered a thing yay."
+echo "INCOMING_HOOK_BODY is $INCOMING_HOOK_BODY"
