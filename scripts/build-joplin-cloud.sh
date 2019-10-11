641720bf86350f7a029e099ba0cab54578c63e8b
Bob Main <bobmain49@gmail.com>
2019-10-11 15:02:51 -0500

    point to staging

--- scripts/build-joplin-cloud.sh
+++ scripts/build-joplin-cloud.sh
@@ -5,7 +5,7 @@ export CMS_API='https://joplin.herokuapp.com/api/graphql'
 export CMS_MEDIA='https://joplin-austin-gov.s3.amazonaws.com/media'
 export CMS_DOCS='multiple'
 
-if [ "$HEAD" == '2906-toc-horizontal-rules' ]; then
+if [ "$HEAD" == '3065-scroll-fix' ]; then
   export CMS_API='https://joplin-staging.herokuapp.com/api/graphql'
 fi
 
@@ -13,9 +13,5 @@ fi
 # if [ "$HEAD" == '2662-service-step-styling' ]; then
 #   export CMS_API='https://joplin-pr-2662-service-step-st.herokuapp.com/api/graphql'
 # fi
-
-if [ "$HEAD" == '2473-preview-topics' ]; then
-  export CMS_API='https://joplin-pr-3082-guide-page-prev.herokuapp.com/api/graphql'
-fi
 
 yarn npm-run-all build-css build-js
