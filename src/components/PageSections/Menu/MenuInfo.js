86c3f2bef5d4b34209ba04482a68a090a86f6ff4
Eric Sherman <ericandrewsherman@gmail.com>
2019-10-16 18:42:48 -0500

    this dosen't seem to work?

--- src/components/PageSections/Menu/MenuInfo.js
+++ src/components/PageSections/Menu/MenuInfo.js
@@ -6,7 +6,7 @@ import CoaSeal from '../../SVGs/CoaSeal';
 import { misc as i18n2 } from 'js/i18n/definitions';
 import { Link } from 'react-static';
 
-const MenuInfo = ({ intl }) => (
+const MenuInfo = ({ handleFullSiteMenuClose, intl }) => (
   <div className="coa-MenuInfo">
     <div className="wrapper container-fluid">
       <div className="coa-MenuInfo__container">
@@ -25,7 +25,11 @@ const MenuInfo = ({ intl }) => (
           <div className="coa-MenuInfo__svg">
             <i className="material-icons">info</i>
           </div>
-          <Link to={'/feedback/'} className="coa-MenuInfo__link">
+          <Link
+            to={'/feedback/'}
+            className="coa-MenuInfo__link"
+            onClick={handleFullSiteMenuClose}
+          >
             {intl.formatMessage(i18n2.giveUserFeedback)}
           </Link>
         </div>
