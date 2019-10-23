24eebef30e585ceba2eb453112412798d4811db0
Brian Smith <briaguya@gmail.com>
2019-10-22 20:39:46 -0500

    clean up guide page urls

--- src/js/queries/getGuidePageQuery.js
+++ src/js/queries/getGuidePageQuery.js
@@ -1,17 +1,88 @@
-import guidePageFragment from './guidePageFragment';
-
 const allGuidePagesQuery = `
   query getGuidePage($id: ID) {
     allGuidePages(id: $id) {
       edges {
         node {
           id
-          ...guidePageInfo
+          title
+          description
+          slug
+          contacts {
+            edges {
+              node {
+                contact {
+                  name
+                  email
+                  phoneNumber {
+                    edges {
+                      node {
+                        id
+                        phoneDescription
+                        phoneNumber
+                      }
+                    }
+                  }
+                  socialMedia
+                  hours {
+                    edges {
+                      node {
+                        dayOfWeek
+                        startTime
+                        endTime
+                      }
+                    }
+                  }
+                  location {
+                    name
+                    street
+                    city
+                    state
+                    zip
+                    country
+                  }
+                }
+              }
+            }
+          }
+          image {
+            id
+            filename
+            title
+          }
+          relatedDepartments {
+            edges {
+              node {
+                relatedDepartment {
+                  id
+                  title
+                  slug
+                }
+              }
+            }
+          }
+          sections {
+            heading
+            pages {
+              url
+              servicePage {
+                title
+                shortDescription
+                additionalContent
+                steps
+                slug
+              }
+              informationPage {
+                title
+                description
+                additionalContent
+                slug
+              }
+            }
+          }
         }
       }
     }
   }
-  ${guidePageFragment}
 `;
 
 export default allGuidePagesQuery;
