c06745af0f5ce80da86ba49779422ccb4824697f
Niicck <niicck@users.noreply.github.com>
2019-10-23 13:01:24 -0500

    dangerously set innerHTML method attempt

--- src/components/Pages/Form.js
+++ src/components/Pages/Form.js
@@ -1,7 +1,7 @@
 import React, { useEffect, Fragment } from 'react';
 import { withRouteData, Head } from 'react-static';
 import { injectIntl } from 'react-intl';
-import path from 'path';
+import axios from 'axios';
 import ReactMarkdown from 'react-markdown';
 
 import { misc as i18n2, services as i18n3 } from 'js/i18n/definitions';
@@ -12,6 +12,8 @@ import ApplicationBlock from 'components/ApplicationBlock';
 import ContactDetails from 'components/Contact/ContactDetails';
 import ContextualNav from 'components/PageSections/ContextualNav';
 import RelatedToMobile from '../PageSections/ContextualNav/RelatedToMobile';
+
+import formHTML from 'components/Pages/testform'
 
 function FormPage({
   formPage: {
@@ -29,20 +31,47 @@ function FormPage({
   },
   intl,
 }) {
-  // <script type="text/javascript" src="https://cityofaustin.formstack.com/forms/js.php/ni_request_day_laborer"></script><noscript><a href="https://cityofaustin.formstack.com/forms/ni_request_day_laborer" title="Online Form">Online Form - NI Request Day Laborer</a></noscript
-  useEffect(() => {
-    const formScript = window.document.createElement('script');
-    formScript.src = "https://cityofaustin.formstack.com/forms/js.php/ni_request_day_laborer";
-    formScript.type = "text/javascript";
-    formScript.async = true;
 
-    const formContainer = window.document.getElementById("coa-FormPage__embedded-form");
-    formContainer.appendChild(formScript);
-
-    return () => {
-      formContainer.removeChild(formScript);
-    }
-  }, [scriptTag]);
+  // // <script type="text/javascript" src="https://cityofaustin.formstack.com/forms/js.php/ni_request_day_laborer"></script><noscript><a href="https://cityofaustin.formstack.com/forms/ni_request_day_laborer" title="Online Form">Online Form - NI Request Day Laborer</a></noscript
+  // useEffect(() => {
+  //   // axios.get("https://cityofaustin.formstack.com/forms/js.php/ni_request_day_laborer",
+  //   //   { headers: {
+  //   //     "Sec-Fetch-Mode": "no-cors",
+  //   //   }
+  //   // })
+  //   // .then(res => {
+  //   //   console.log("What I get?", res.data)
+  //   // })
+  //   // .catch(error => {
+  //   //   console.log("Ohhh no.")
+  //   // })
+  //
+  //   const formScript = window.document.createElement('script');
+  //   formScript.src = "https://cityofaustin.formstack.com/forms/js.php/ni_request_day_laborer";
+  //   // formScript.innerHTML = "console.log('YYOOOOOO');console.log('durrrrp')"
+  //   formScript.type = "text/javascript";
+  //   formScript.async = true;
+  //
+  //   const dangerousInnerHTML = "";
+  //   // const actualForm = window.document.createElement('script');
+  //   // actualForm.innerHTML = ""
+  //   // actualForm.type = "text/javascript";
+  //   // actualForm.async = true;
+  //
+  //   const formContainer = window.document.getElementById("coa-FormPage__embedded-form");
+  //   document.write = (stuff) => {
+  //     console.log(stuff)
+  //     dangerousInnerHTML = dangerousInnerHTML + stuff
+  //   }
+  //   console.log("~~~~~~~~~~~~~~~~~~~")
+  //   formContainer.appendChild(formScript);
+  //   console.log("~~~~~~~~~~~~~~~~~~~")
+  //   formContainer.appendChild(actualForm);
+  //
+  //   return () => {
+  //     formContainer.removeChild(formScript);
+  //   }
+  // }, [scriptTag]);
 
   return (
     <div>
@@ -66,6 +95,7 @@ function FormPage({
         <div className="coa-Page__all-of-the-content">
           <div className="coa-Page__main-content">
             <div id="coa-FormPage__embedded-form">
+              <div dangerouslySetInnerHTML={{__html: formHTML}}/>
             </div>
           </div>
           <div className="coa-Page__side-content">
