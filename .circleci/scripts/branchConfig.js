f051b3314ad5e345036568128032877047d831be
Niicck <niicck@users.noreply.github.com>
2019-10-25 13:59:19 -0500

    add CMS_API for 3202-form

--- .circleci/scripts/branchConfig.js
+++ .circleci/scripts/branchConfig.js
@@ -27,6 +27,9 @@ const branchOverrides = {
   "2997-dept-links": {
     CMS_API: "https://joplin-pr-2997-dept-links.herokuapp.com/api/graphql"
   },
+  "3202-form": {
+    CMS_API: "https://joplin-pr-3202-form.herokuapp.com/api/graphql"
+  }
 }
 
 module.exports = {
