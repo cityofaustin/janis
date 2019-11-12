533ea8c1d719b45354102848623bd80e5b127971
Bob Main <bobmain49@gmail.com>
2019-11-11 18:36:48 -0600

    changing branch api

--- .circleci/scripts/branchConfig.js
+++ .circleci/scripts/branchConfig.js
@@ -36,7 +36,7 @@ const branchOverrides = {
   },
   '3163-guide-espanol': {
     CMS_API: 'https://joplin-pr-3163-guide-pages.herokuapp.com/api/graphql'
-  }
+  },
   '3244-guide-icon-tiles': {
     CMS_API: 'https://joplin-pr-3244-guide-icon-tile.herokuapp.com/api/graphql'
   }
