eff65eeb78a066c515120a5ce1b7d845064ba64d
Brian Smith <briaguya@gmail.com>
2019-11-15 13:47:34 -0600

    get rid of weird bottom margin

--- src/components/HtmlFromAdmin/index.js
+++ src/components/HtmlFromAdmin/index.js
@@ -42,6 +42,12 @@ const HtmlFromAdmin = ({ content }) => {
           domNode.attribs.title = 'Embedded video';
         }
 
+        if (domNode.attribs.class === 'responsive-object') {
+          // For some reason wagtail chucks a giant margin on the bottom here,
+          // let's clear that out and handle any additional styling using classes
+          domNode.attribs.style = '';
+        }
+
         // Turn links into buttons
         // waiting on
         // if (
