9a8a9110ea6286fba380f58b79169e273d045aa8
Brian Smith <briaguya@gmail.com>
2019-12-24 17:54:31 -0500

    little bit of query cleanup

--- src/js/queries/getDepartmentPageQuery.js
+++ src/js/queries/getDepartmentPageQuery.js
@@ -1,113 +1,16 @@
+import departmentPageFragment from './departmentPageFragment';
+
 const getDepartmentPageQuery = `
   query getDepartmentPage($id: ID) {
     allDepartmentPages(id: $id) {
       edges {
         node {
-          id
-          slug
-          title
-          whatWeDo
-          image {
-            id
-            filename
-            title
-          }
-          mission
-          departmentDirectors {
-            edges {
-              node {
-                name
-                photo {
-                  id
-                  filename
-                }
-                about
-                title
-              }
-            }
-          }
-          jobListings
-          topPages {
-            edges {
-              node {
-                pageId
-                slug
-                title
-              }
-            }
-          }
-          relatedPages {
-            edges {
-              node {
-                pageId
-                slug
-                title
-              }
-            }
-          }
-          contacts {
-            edges {
-              node {
-                contact {
-                  name
-                  phoneNumber {
-                    edges {
-                      node {
-                        id
-                        phoneDescription
-                        phoneNumber
-                      }
-                    }
-                  }
-                  email
-                  socialMedia
-                  locationPage {
-                    id
-                    title
-                    slug
-                    physicalStreet
-                    physicalUnit
-                    physicalCity
-                    physicalState
-                    physicalCountry
-                    physicalZip
-                    mondayStartTime
-                    mondayEndTime
-                    mondayStartTime2
-                    mondayEndTime2
-                    tuesdayStartTime
-                    tuesdayEndTime
-                    tuesdayStartTime2
-                    tuesdayEndTime2
-                    wednesdayStartTime
-                    wednesdayEndTime
-                    wednesdayStartTime2
-                    wednesdayEndTime2
-                    thursdayStartTime
-                    thursdayEndTime
-                    thursdayStartTime2
-                    thursdayEndTime2
-                    fridayStartTime
-                    fridayEndTime
-                    fridayStartTime2
-                    fridayEndTime2
-                    saturdayStartTime
-                    saturdayEndTime
-                    saturdayStartTime2
-                    saturdayEndTime2
-                    sundayStartTime
-                    sundayEndTime
-                    sundayStartTime2
-                    sundayEndTime2
-                  }
-                }
-              }
-            }
-          }
+          ...departmentPageInfo
         }
       }
     }
   }
+  ${departmentPageFragment}
 `;
 
 export default getDepartmentPageQuery;
