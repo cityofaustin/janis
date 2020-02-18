2e0a91a45bba7331d146445492c05d4c420cfcf7
Chia Berry <chia.berry@austintexas.gov>
2020-02-18 11:49:15 -0600

    branch config

--- .circleci/scripts/branchConfig.js
+++ .circleci/scripts/branchConfig.js
@@ -25,8 +25,8 @@ const defaultValues = {
 
 // Add branch-specifc values here
 const branchOverrides = {
-  "3470-map-address": {
-    CMS_API: "https://joplin-pr-3227-permissions-mvp.herokuapp.com/api/graphql"
+  "3817-event-lists": {
+    CMS_API: "https://joplin-pr-3817-event-lists.herokuapp.com/api/graphql"
   },
   '1650-react-static-7': {
     CMS_API: 'https://joplin-pr-brians-janis-testing.herokuapp.com/api/graphql',
