15e326665de5b95644d2383db2b6f8d0ff4d18ec
Chia Berry <chia.berry@austintexas.gov>
2020-02-18 21:01:51 -0600

    filter on date

--- src/components/Pages/EventList/index.js
+++ src/components/Pages/EventList/index.js
@@ -8,10 +8,17 @@ import EventListPagination from 'components/Pages/EventList/EventListPagination'
 import UserFeedback from 'components/UserFeedback';
 import { events as i18n } from 'js/i18n/definitions';
 
+const filterOnDate = event => {
+  // the api returns events greater than or equal to the time when the site is built
+  // this filters out the ones between the build time and now
+  const dateNow = moment().format('YYYY-MM-DD')
+  return moment(event.date).isSameOrAfter(dateNow);
+}
 
 const EventList = () => {
   const intl = useIntl();
   const { events } = useRouteData();
+
   return (
     <div>
       <Head>
@@ -19,7 +26,7 @@ const EventList = () => {
       </Head>
       <PageHeader contentType={'event-list'}> {intl.formatMessage(i18n.events)} </PageHeader>
       <div className="wrapper container-fluid">
-        <EventListPagination events={events} intl={intl} />
+        <EventListPagination events={events.filter(filterOnDate)} intl={intl} />
         <UserFeedback />
       </div>
     </div>
