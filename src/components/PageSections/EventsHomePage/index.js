eb69f939f5e142e6fad73e0332808b019f5fc150
Chia Berry <chia.berry@austintexas.gov>
2020-02-21 17:29:19 -0600

    more spanish

--- src/components/PageSections/EventsHomePage/index.js
+++ src/components/PageSections/EventsHomePage/index.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import moment from 'moment-timezone';
 
 import EventListEntry from 'components/Pages/EventList/EventListEntry';
+import { events as i18n } from 'js/i18n/definitions';
 
 const filterEvents = (events) => {
   const dateNow = moment().format('YYYY-MM-DD')
@@ -19,7 +20,7 @@ const EventsHomePage = ({events}) => {
     threeEvents && threeEvents.length &&
     <div className="coa-EventsHomePage__container">
       <div className="coa-EventsHomePage__heading">
-        Upcoming events
+        {intl.formatMessage(i18n.upcoming)}
       </div>
 
       <div className="coa-EventsHomePage__events">
@@ -33,7 +34,7 @@ const EventsHomePage = ({events}) => {
         className="coa-EventsHomePage__allEventsButton"
       >
         <div className="coa-EventsHomePage__allEventsButton--inner">
-          View all events 
+          {intl.formatMessage(i18n.viewAll)}
         </div>
       </Link>
 
