e3597805ba4c48f95af1be0868ad0031433e330d
Chia Berry <chia.berry@austintexas.gov>
2020-02-25 21:13:55 -0600

    only get live events

--- src/js/queries/getEventPageQuery.js
+++ src/js/queries/getEventPageQuery.js
@@ -2,7 +2,7 @@ import eventPageFragment from './eventPageFragment';
 
 const getEventPageQuery = `
   query getEventPage($id: ID, $date_Lte: Date, $date_Gte: Date, $canceled: Boolean) {
-    allEventPages(orderBy:"date", id:$id, date_Lte:$date_Lte, date_Gte:$date_Gte, canceled: $canceled) {
+    allEventPages(orderBy:"date", id:$id, date_Lte:$date_Lte, date_Gte:$date_Gte, canceled: $canceled, live:true) {
       edges {
         node {
           ...eventPageInfo
