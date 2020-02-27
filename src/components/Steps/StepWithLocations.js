7ef079d4c63e1ba4f39da93981a469c5a15cfd97
Brian Smith <briaguya@gmail.com>
2020-02-27 13:50:02 -0600

    bonus address formatting

--- src/components/Steps/StepWithLocations.js
+++ src/components/Steps/StepWithLocations.js
@@ -15,13 +15,23 @@ const LocationInStep = ({ location }) => {
         <p className="coa-StepWithLocations__location-title">
           {location.locationPage.title}
         </p>
-        <p className="coa-StepWithLocations__location-address">{`${
+        <p className="coa-StepWithLocations__location-address--singleline">{`${
           location.locationPage.physicalStreet
         }${location.locationPage.physicalUnit}, ${
           location.locationPage.physicalCity
         }, ${location.locationPage.physicalState} ${
           location.locationPage.physicalZip
         }`}</p>
+        <p className="coa-StepWithLocations__location-address--doubleline">
+          {`${location.locationPage.physicalStreet}${
+            location.locationPage.physicalUnit
+          }`}
+        </p>
+        <p className="coa-StepWithLocations__location-address--doubleline">
+          {`${location.locationPage.physicalCity}, ${
+            location.locationPage.physicalState
+          } ${location.locationPage.physicalZip}`}
+        </p>
       </div>
       <div className="coa-StepWithLocations__location-arrow">
         <i class="material-icons">arrow_forward</i>
