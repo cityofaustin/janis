e2649f6acb58e6458ed201f90bf986386e54b29f
Chia Berry <chia.berry@austintexas.gov>
2020-04-16 11:56:59 -0500

    tabs

--- src/js/queries/departmentPageFragment.js
+++ src/js/queries/departmentPageFragment.js
@@ -44,58 +44,58 @@ const departmentPageFragment = `
       }
     }
     contact {
-   name
-    phoneNumbers {
-      edges {
-        node {
-          id
-          phoneDescription
-          phoneNumber
+     name
+      phoneNumbers {
+        edges {
+          node {
+            id
+            phoneDescription
+            phoneNumber
+          }
         }
       }
-    }
-    email
-    socialMedia
-    locationPage {
-      id
-      title
-      slug
-      physicalStreet
-      physicalUnit
-      physicalCity
-      physicalState
-      physicalCountry
-      physicalZip
-      mondayStartTime
-      mondayEndTime
-      mondayStartTime2
-      mondayEndTime2
-      tuesdayStartTime
-      tuesdayEndTime
-      tuesdayStartTime2
-      tuesdayEndTime2
-      wednesdayStartTime
-      wednesdayEndTime
-      wednesdayStartTime2
-      wednesdayEndTime2
-      thursdayStartTime
-      thursdayEndTime
-      thursdayStartTime2
-      thursdayEndTime2
-      fridayStartTime
-      fridayEndTime
-      fridayStartTime2
-      fridayEndTime2
-      saturdayStartTime
-      saturdayEndTime
-      saturdayStartTime2
-      saturdayEndTime2
-      sundayStartTime
-      sundayEndTime
-      sundayStartTime2
-      sundayEndTime2
-      hoursExceptions
-    }
+      email
+      socialMedia
+      locationPage {
+        id
+        title
+        slug
+        physicalStreet
+        physicalUnit
+        physicalCity
+        physicalState
+        physicalCountry
+        physicalZip
+        mondayStartTime
+        mondayEndTime
+        mondayStartTime2
+        mondayEndTime2
+        tuesdayStartTime
+        tuesdayEndTime
+        tuesdayStartTime2
+        tuesdayEndTime2
+        wednesdayStartTime
+        wednesdayEndTime
+        wednesdayStartTime2
+        wednesdayEndTime2
+        thursdayStartTime
+        thursdayEndTime
+        thursdayStartTime2
+        thursdayEndTime2
+        fridayStartTime
+        fridayEndTime
+        fridayStartTime2
+        fridayEndTime2
+        saturdayStartTime
+        saturdayEndTime
+        saturdayStartTime2
+        saturdayEndTime2
+        sundayStartTime
+        sundayEndTime
+        sundayStartTime2
+        sundayEndTime2
+        hoursExceptions
+      }
     }
   }
 `;
