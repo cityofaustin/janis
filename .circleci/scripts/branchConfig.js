f027061e6fa5467314d58500bd9be825bb25bc35
Chia Berry <chia.berry@austintexas.gov>
2020-04-16 21:05:14 -0500

    branchconfig

--- .circleci/scripts/branchConfig.js
+++ .circleci/scripts/branchConfig.js
@@ -39,7 +39,7 @@ const branchOverrides = {
     CMS_API: 'https://joplin-pr-3690-incremental.herokuapp.com/api/graphql',
     REACT_STATIC_PREFETCH_RATE: '10',
 },
-  '4185-make-janis-build': {
+  '4289-page-guide': {
     CMS_API: 'https://joplin-pr-v3.herokuapp.com/api/graphql'
   }
 };
