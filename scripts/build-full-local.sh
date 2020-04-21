11635aa9884fcd93aead6d92c554e109ec122ef1
Niicck <niicck@users.noreply.github.com>
2020-04-21 15:31:12 -0500

    allow local full site builds on v3 Joplin

---
+++ scripts/build-full-local.sh
@@ -0,0 +1,24 @@
+# Add your Joplin PR API and Media Links here...
+# - NOTE: make sure CMS_API has `/api/graphiql` endpoints
+# - AND... CMS_MEDIA has `/media` endpoints
+
+# Examples
+# - export CMS_API='https://joplin-pr-3244-guide-icon-tile.herokuapp.com/api/graphql'
+# - export CMS_MEDIA='https://joplin-pr-3244-guide-icon-tile.herokuapp.com/media'
+
+# Or,  maybe you want to use staging or production media
+# - Staging Media: `https://joplin-austin-gov-static.s3.amazonaws.com/staging/media`
+# - Production Media: `https://joplin-austin-gov-static.s3.amazonaws.com/production/media`
+
+export NODE_PATH='./src'
+
+export REACT_STATIC_PREFETCH_RATE=5
+# Must be 127.0.0.1, not localhost
+export CMS_API='http://127.0.0.1:8000/api/graphql'
+export CMS_MEDIA='http://127.0.0.1:8000/media'
+
+yarn npm-run-all build-css build-js
+
+echo " 🏗 END of the Joplin PR Build 🏗 "
+
+http-server dist
