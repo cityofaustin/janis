c300c95637eff137f2d852ac88c58c7b1dfcd40f
Brian Smith <briaguya@gmail.com>
2020-04-22 09:09:19 -0500

    build against import everything

--- scripts/build-full.sh
+++ scripts/build-full.sh
@@ -12,7 +12,7 @@
 
 export NODE_PATH='./src'
 
-export CMS_API='https://joplin-staging.herokuapp.com/api/graphql'
+export CMS_API='https://joplin-pr-import-everything.herokuapp.com/api/graphql'
 export CMS_MEDIA='https://joplin-austin-gov-static.s3.amazonaws.com/staging/media'
 
 yarn npm-run-all build-css build-js
