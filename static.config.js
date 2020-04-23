ce64cc351579c77c45f3be202bdee7f60816f191
Chia Berry <chia.berry@austintexas.gov>
2020-04-23 09:31:46 -0500

    remove polyfill

--- static.config.js
+++ static.config.js
@@ -741,17 +741,6 @@ const makeAllPages = async (langCode, incrementalPageId) => {
     }),
   );
 
-  // polyfill
-  if (!Array.prototype.flat) {
-    Object.defineProperty(Array.prototype, 'flat', {
-      value: function(depth = 1) {
-        return this.reduce(function (flat, toFlatten) {
-          return flat.concat((Array.isArray(toFlatten) && (depth>1)) ? toFlatten.flat(depth-1) : toFlatten);
-        }, []);
-      }
-    });
-  }
-
   // the nested maps return nested arrays that need to be flattened
   allPages = allPages.flat();
 
