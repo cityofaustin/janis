33ff222547431590a043dbebcafc03a09f7283dd
Chia Berry <chia.berry@austintexas.gov>
2020-04-29 22:03:08 -0500

    locations can be empty

--- src/js/helpers/cleanData.js
+++ src/js/helpers/cleanData.js
@@ -525,7 +525,7 @@ export const cleanEvents = events => {
       eventUrl: getEventPageUrl(event.slug, event.date),
       feesRange: formatFeesRange(event.fees),
       // until we have support for multiple locations, we're taking the first one
-      location: event.locations[0],
+      location: event.locations && event.locations.length ? event.locations[0] : null,
       eventIsFree: event.eventIsFree,
       registrationUrl: event.registrationUrl,
     }
