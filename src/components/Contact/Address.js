db9d9aad3468bd82a3d1ee6e89ffc5691ac541f3
Bob Main <bobmain49@gmail.com>
2020-04-30 16:14:02 -0500

    forgot it should use state too

--- src/components/Contact/Address.js
+++ src/components/Contact/Address.js
@@ -9,7 +9,7 @@ import { addressPropTypes } from './proptypes';
 const Address = ({ location }) => {
   const intl = useIntl();
   const encodedLocation = getEncodedLocation(location);
-
+  
   return (
     <div className="coa-ContactItem coa-ContactAddress">
       <i className="material-icons">place</i>
@@ -17,7 +17,7 @@ const Address = ({ location }) => {
         <span className="coa-ContactAddressTitle">{location.title}</span>
         <span>{location.street}</span>
         <span>
-          {location.city}{location.city && ", "}{location.state} {location.zip}
+          {location.city}{ (location.city && location.state) && ", "}{location.state} {location.zip}
         </span>
         <a
           href={`//www.google.com/maps/search/?api=1&query=${encodedLocation}`}
