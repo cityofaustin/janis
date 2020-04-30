1b66b36dcb86f51d5a23632867608beb0cead4ba
Bob Main <bobmain49@gmail.com>
2020-04-30 18:11:49 -0500

    addition location info if it does have a city.

--- src/components/Contact/ContactDetails.js
+++ src/components/Contact/ContactDetails.js
@@ -49,9 +49,8 @@ const ContactDetailsEntry = ({
       <PhonesList phoneNumbers={phoneNumbers} />
     )}
     {email && <Email email={email} />}
-    {/* Removing incomplete location details 
-      {location && <Address location={location} />}
-    */}
+    {/* Only if a location has a city should we pass the location info comopoonent in. */}
+    { (location && location.city) && <Address location={location} />}
     {hours && !!hours.length && <Hours hours={hours} />}
     {/*Each social media link is it's own contact item with it's own icon,
     so it makes sense to have the map here*/}
