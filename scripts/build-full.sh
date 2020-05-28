48be96c5adb890742cb5ec993840f63a4c1a792b
Brian Smith <briaguya@gmail.com>
2020-05-27 22:58:50 -0500

    point to the right joplin

--- scripts/build-full.sh
+++ scripts/build-full.sh
@@ -12,8 +12,8 @@
 
 export NODE_PATH='./src'
 
-export CMS_API='https://joplin-pr-4404-more-news-test.herokuapp.com/api/graphql'
-export CMS_MEDIA='https://joplin3-austin-gov-static.s3.amazonaws.com/staging/media'
+export CMS_API='https://joplin-pr-4404-news.herokuapp.com/api/graphql'
+export CMS_MEDIA='https://joplin3-austin-gov-static.s3.amazonaws.com/review/4404-news/media'
 
 yarn npm-run-all build-css build-js
 
