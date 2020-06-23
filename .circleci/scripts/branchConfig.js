2aa8100d15ce0b3ffb8ed9cebb939334629d435f
Bob Main <bobmain49@gmail.com>
2020-06-23 18:40:39 -0500

    we shall try again

--- .circleci/scripts/branchConfig.js
+++ .circleci/scripts/branchConfig.js
@@ -33,7 +33,7 @@ const branchOverrides = {
   '4289-page-guide': {
     joplin_appname: 'joplin-pr-v3',
   },
-  'dummy2-bob': { // note
+  '4392-nav-v2': { 
     joplin_appname: 'joplin'
   },
   '4422-20-to-10': {
