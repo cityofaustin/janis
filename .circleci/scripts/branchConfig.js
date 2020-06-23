7d736aa79fb9e60ea0fd0c8256b62dbe08ebea02
Bob Main <bobmain49@gmail.com>
2020-06-23 16:48:50 -0500

    rebuild test bump

--- .circleci/scripts/branchConfig.js
+++ .circleci/scripts/branchConfig.js
@@ -33,7 +33,7 @@ const branchOverrides = {
   '4289-page-guide': {
     joplin_appname: 'joplin-pr-v3',
   },
-  'nav2': {
+  'nav2': { // test
     joplin_appname: 'joplin'
   },
   '4422-20-to-10': {
