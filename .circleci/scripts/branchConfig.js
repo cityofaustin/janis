72e4c7da115d87564b07c4b61c72c95ea8449faf
Bob Main <bobmain49@gmail.com>
2020-07-29 20:43:09 -0500

    initial commit

--- .circleci/scripts/branchConfig.js
+++ .circleci/scripts/branchConfig.js
@@ -33,7 +33,7 @@ const branchOverrides = {
   '4289-page-guide': {
     joplin_appname: 'joplin-pr-v3',
   },
-  '4392-nav-v2': { // Ok to remove
+  'smaller-seal': { // Ok to remove
     joplin_appname: 'joplin'
   },
   '4591-janis-docs': {
