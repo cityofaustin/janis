df18d761c9f75b72299d3d65a0f37a672e6ddb78
Chia Berry <chia.berry@austintexas.gov>
2020-12-07 16:48:12 -0600

    style the link

--- src/components/Alerts/locationsCOVID_19.js
+++ src/components/Alerts/locationsCOVID_19.js
@@ -2,26 +2,28 @@ import React from 'react';
 import { useIntl } from 'react-intl';
 import { alert as i18n1 } from 'js/i18n/definitions';
 
-const LocationsCOVID_19 = ({description}) => {
+const LocationsCOVID_19 = ({ description }) => {
   const intl = useIntl();
   return (
     <div>
       <div className="coa-LocationAlert__block">
         {intl.formatMessage(i18n1.location)}
       </div>
-      <div className="coa-LocationAlert__block">
-        <a
-          href="https://www.austintexas.gov/COVID19"
-          className="coa-HomepageAlert__link-external"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <a
+        href="https://www.austintexas.gov/COVID19"
+        className="coa-LocationAlert__link-external"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="coa-LocationAlert__block">
           {description}
-          <i className="material-icons coa-HomepageAlert__link-icon">open_in_new</i>
-        </a>
-      </div>
+          <i className="material-icons coa-LocationAlert__link-icon">
+            open_in_new
+          </i>
+        </div>
+      </a>
     </div>
-  )
-}
+  );
+};
 
 export default LocationsCOVID_19;
