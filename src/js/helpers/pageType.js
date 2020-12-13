9b2a0790da9bcb86ef0b3425e69ab67accccd7f2
Chia Berry <chia.berry@austintexas.gov>
2020-12-13 12:48:37 -0600

    check if exists

--- src/js/helpers/pageType.js
+++ src/js/helpers/pageType.js
@@ -1,6 +1,11 @@
-export const isMuniCourt = (contextualNavData) => {
-  if (contextualNavData.offeredBy && contextualNavData.offeredBy[0].url === "/municipal-court/") {
-    return true
+export const isMuniCourt = contextualNavData => {
+  console.log(!!contextualNavData.offeredBy.length)
+  console.log(contextualNavData.offeredBy[0])
+  if (
+    !!contextualNavData.offeredBy.length &&
+    contextualNavData.offeredBy[0].url === '/municipal-court/'
+  ) {
+    return true;
   }
-  return false
-} 
+  return false;
+};
