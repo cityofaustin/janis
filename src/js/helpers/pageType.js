142a10a0e436d48614ba7a9c7a0ae926340dc13f
Chia Berry <chia.berry@austintexas.gov>
2020-12-13 12:49:29 -0600

    clean up

--- src/js/helpers/pageType.js
+++ src/js/helpers/pageType.js
@@ -1,6 +1,4 @@
 export const isMuniCourt = contextualNavData => {
-  console.log(!!contextualNavData.offeredBy.length)
-  console.log(contextualNavData.offeredBy[0])
   if (
     !!contextualNavData.offeredBy.length &&
     contextualNavData.offeredBy[0].url === '/municipal-court/'
