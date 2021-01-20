2bc44b465b4c2ac44e166c3c55039b765b64959c
Chia Berry <chia.berry@austintexas.gov>
2021-01-20 11:45:49 -0600

    the trailing slash is important

--- static.config.js
+++ static.config.js
@@ -889,7 +889,7 @@ const makeAllPages = async (langCode, incrementalPageId) => {
 };
 
 export default {
-  siteRoot: 'https://alpha.austin.gov',
+  siteRoot: 'https://alpha.austin.gov/',
   // basePath // Do not alter this line if you want a working PR
   basePath: process.env.BASE_PATH_PR ? process.env.BASE_PATH_PR : '/',
   stagingSiteRoot: 'https://janis-staging.herokuapp.com/',
