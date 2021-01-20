997846f63dc8baaf317d310f22adce1b00729863
Chia Berry <chia.berry@austintexas.gov>
2021-01-20 12:36:41 -0600

    remove siteroot

--- static.config.js
+++ static.config.js
@@ -889,10 +889,10 @@ const makeAllPages = async (langCode, incrementalPageId) => {
 };
 
 export default {
-  siteRoot: 'https://alpha.austin.gov/',
+  // siteRoot: 'https://alpha.austin.gov/',
   // basePath // Do not alter this line if you want a working PR
   basePath: process.env.BASE_PATH_PR ? process.env.BASE_PATH_PR : '/',
-  // stagingSiteRoot: 'https://janis-staging.herokuapp.com/',
+  stagingSiteRoot: 'https://janis-staging.herokuapp.com/',
   getSiteProps: () => ({
     title: 'City of Austin',
   }),
